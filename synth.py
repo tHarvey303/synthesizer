156feb6aeda5aae148e213d0987eef3a2a9c2947
Will Roper <wjr21@sussex.ac.uk>
2022-05-18 00:35:19 +0100

    Suggested function

--- synth.py
+++ synth.py
@@ -12,6 +12,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("param_file", help="parameter file", type=str)
 args = parser.parse_args()
 
+# Will: Could we bury this in a function in utils called "read_params" or similar
+# would aide readability but could also let us hide other manipulations we had to do
+# an example being if we have multiple levels of verbosity on different ranks 
+# comes to mind but might be other stuff in the long run.
 params = __import__(args.param_file)
 
 print(params.sps_grid)
