ce2d61c96e17ea53f74c020ec130f72d2ceef162
christopherlovell <chrisl1991@hotmail.co.uk>
2022-05-18 13:49:21 +0100

    initial load_data script

---
+++ synthesizer/load_data.py
@@ -0,0 +1,41 @@
+import sys
+import h5py
+import numpy as np
+
+
+def load_FLARES(f, tag):
+    with h5py.File(f, 'r') as hf:
+        lens = hf[f'{tag}/Galaxy/S_Length'][:]
+        ages = hf[f'{tag}/Particle/S_Age'][:]
+        coods = hf[f'{tag}/Particle/S_Coordinates'][:]
+        mass = hf[f'{pre}/S_Mass'][:]
+        imass = hf[f'{pre}/S_MassInitial'][:]
+        metals = hf[f'{pre}/S_Z'][:]
+        # ids = hf[f'{tag}/Particle/S_ID'][:]
+        # index = hf[f'{tag}/Particle/S_Index'][:]
+        # hf[f'{pre}/S_Vel']
+        # hf[f'{pre}/S_Z_smooth']
+
+    begin, end = get_len(lens)
+
+    # TODO: assign arrays to star particle objects, and subsequently galaxy objects
+
+
+def get_len(Length):
+    begin = np.zeros(len(Length), dtype = np.int64)
+    end = np.zeros(len(Length), dtype = np.int64)
+    begin[1:] = np.cumsum(Length)[:-1]
+    end = np.cumsum(Length)
+    return begin, end
+
+
+def main():
+    # e.g. '/cosma7/data/dp004/dc-love2/codes/flares/data/FLARES_30_sp_info.hdf5'
+    _f = sys.argv[1]
+    tag = sys.argv[2] # '/010_z005p000/'
+    load_FLARES(_f, tag)
+    return None
+
+
+if __name__ == "__main__":
+    main()
