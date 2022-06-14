842ac0138586ddb35bac934c0a0d24939e9d90cf
christopherlovell <chrisl1991@hotmail.co.uk>
2022-06-14 08:06:50 +0100

    added resample stars method

--- synthesizer/load_data.py
+++ synthesizer/load_data.py
@@ -9,12 +9,12 @@ def load_FLARES(f, region, tag):
     with h5py.File(f, 'r') as hf:
         lens = hf[f'{region}/{tag}/Galaxy/S_Length'][:]
         ages = hf[f'{region}/{tag}/Particle/S_Age'][:]  # Gyr
-        coods = hf[f'{region}/{tag}/Particle/S_Coordinates'][:]
+        coods = hf[f'{region}/{tag}/Particle/S_Coordinates'][:].T
         mass = hf[f'{region}/{tag}/Particle/S_Mass'][:]  # 1e10 Msol
-        # imass = hf[f'{tag}/Particle/S_MassInitial'][:]
+        imass = hf[f'{region}/{tag}/Particle/S_MassInitial'][:]  # 1e10 Msol
         metals = hf[f'{region}/{tag}/Particle/S_Z'][:]
-        # ids = hf[f'{tag}/Particle/S_ID'][:]
-        # index = hf[f'{tag}/Particle/S_Index'][:]
+        # ids = hf[f'{region}/{tag}/Particle/S_ID'][:]
+        # index = hf[f'{region}/{tag}/Particle/S_Index'][:]
         # hf[f'{pre}/S_Vel']
         # hf[f'{pre}/S_Z_smooth']
 
@@ -22,13 +22,16 @@ def load_FLARES(f, region, tag):
     # ages = np.log10(ages * 1e9)  # log10(yr)
     ages = (ages * 1e9)  # log10(yr)
     mass = mass * 1e10  # Msol
+    imass = imass * 1e10  # Msol
 
     begin, end = get_len(lens)
 
     galaxies = [None] * len(begin)
     for i, (b, e) in enumerate(zip(begin, end)):
         galaxies[i] = Galaxy()
-        galaxies[i].load_stars(mass[b:e], ages[b:e], metals[b:e], coordinates=coods[:,b:e])
+        galaxies[i].load_stars(mass[b:e], ages[b:e], metals[b:e], 
+                               coordinates=coods[b:e, :],
+                               initial_mass=imass[b:e])
 
     return galaxies
 
