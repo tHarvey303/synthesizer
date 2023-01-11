f2822359f03c7a4893db8d776375bea716b5f46c
wjr21 <wjr21@sussex.ac.uk>
2023-01-11 19:14:07 +0000

    Added error for inconsistent arguments and reformulated logic

--- synthesizer/exceptions.py
+++ synthesizer/exceptions.py
@@ -1,7 +1,9 @@
 
 
-
 class InconsistentParameter(Exception):
+    """ Generic exception class for inconsistent parameters.
+    """
+
     def __init__(self, *args):
         """
         Exception raised for inconsistent parameter choice
@@ -16,5 +18,25 @@ class InconsistentParameter(Exception):
     def __str__(self):
         if self.message:
             return '{0} '.format(self.message)
+        return 'Inconsistent parameter choice'
+
+
+class InconsistentArguments(Exception):
+    """ Generic exception class for inconsistent combinations of arguments.
+    """
+
+    def __init__(self, *args):
+        """
+        Exception raised for inconsistent argument choice
+        :param expression:
+        :param message:
+        """
+        if args:
+            self.message = args[0]
         else:
-            return 'Inconsistent parameter choice'
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return '{0} '.format(self.message)
+        return 'Inconsistent parameter choice'
