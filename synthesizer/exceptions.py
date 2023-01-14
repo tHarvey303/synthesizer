a6bd4a676d2b1db8cfab562aa05ff05125c453de
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-01-14 11:32:37 +0000

    improved Sed class

--- synthesizer/exceptions.py
+++ synthesizer/exceptions.py
@@ -17,3 +17,22 @@ class InconsistentParameter(Exception):
             return '{0} '.format(self.message)
         else:
             return 'Inconsistent parameter choice'
+
+
+class InconsistentAddition(Exception):
+    def __init__(self, *args):
+        """
+        Exception raised for inconsistent adding (a+b=c)
+        :param expression:
+        :param message:
+        """
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return '{0} '.format(self.message)
+        else:
+            return 'Unable to add'
