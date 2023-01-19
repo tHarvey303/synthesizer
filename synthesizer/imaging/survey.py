80e5d80bc10d749ae650cb99e23b2f6fd7657ec3
wjr21 <wjr21@sussex.ac.uk>
2023-01-19 11:41:21 +0000

    Added Skeleton for survey

--- synthesizer/imaging/survey.py
+++ synthesizer/imaging/survey.py
@@ -1,2 +1,74 @@
+"""
+Survey functionality 
+"""
+
+class Observatory:
+    """
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+
+    """
+
+    def __init__(self, resolution, filters=(), psfs=()):
+        """
+        Initialise the Observatory.
+
+        Parameters
+        ----------
+
+        """
+
+        # Attributes for the
+
+    def _check_observatory_args(self):
+        """
+        Ensures we have valid inputs.
+
+        Parameters
+        ----------
+        
+        Raises
+        ------
+        
+        """
+
+        
 class Survey:
-    pass
+    """
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+
+    """
+    
+    def __init__(self, observatories, depths=None, aperture=None):
+        """
+        Initialise the Survey.
+
+        Parameters
+        ----------
+
+        """
+
+        # Noise related attributes
+
+
+    def _check_survey_args(self):
+        """
+        Ensures we have valid inputs.
+
+        Parameters
+        ----------
+        
+        Raises
+        ------
+        
+        """
+        
