84f8217b793ba707ce317ac4b14005c8fda2a9f3
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-01-19 15:11:20 +0000

    added unit functionality

--- synthesizer/units.py
+++ synthesizer/units.py
@@ -1,23 +1,49 @@
 
-
-import unyt
+from dataclasses import dataclass
 from unyt import nJy, erg, s, Hz, Angstrom, cm
 
 
+@dataclass
 class Units:
+
+    """
+    Holds the definition of the internal unit system using unyt.
+    """
+
     wavelength = Angstrom
-    lnu = erg/s/Hz
-    luminosity = erg/s
-    flux = erg/s/cm**2
+
+    luminosity = erg/s  # luminosity
+    lnu = erg/s/Hz  # spectral luminosity density
+    llam = erg/s/Angstrom  # spectral luminosity density
     continuum = erg/s/Hz  # the continuum level of an emission line
+
+    flux = erg/s/cm**2
     fnu = nJy
-    ew = Angstrom
+
+    ew = Angstrom  # equivalent width
 
 
 class Quantity:
 
+    """
+    Provides the ability to associate values with the unyt definition of the internal unit system to output quantities.
+
+
+    Methods
+    -------
+    __init__
+        Initialise and set Units
+    __set_name__
+        Define public and private names
+    __get__
+        Return the quantity, a combination of the (unit-less) value and the unit
+    __set__
+        Set the value.
+
+    """
+
     def __init__(self):
-        self.units = Units()
+        self.units = Units()  # I suppose this could be a parameter allowing you to change the implementation of units, e.g. swapping to astropy
 
     def __set_name__(self, owner, name):
         self.public_name = name
@@ -30,29 +56,3 @@ class Quantity:
 
     def __set__(self, obj, value):
         setattr(obj, self.private_name, value)
-
-
-# class Property:
-#
-#     def __init__(self):
-#         self.units = {
-#             'wavelength': Angstrom,
-#             'lnu': erg/s,
-#             'luminosity': erg/s,
-#             'flux': erg/s/cm**2,
-#             'lnu': erg/s/Hz,
-#             'continuum': erg/s/Hz,  # the continuum level of an emission line
-#             'fnu': nJy,
-#             'ew': Angstrom,
-#         }
-#
-#     def __set_name__(self, owner, name):
-#         self.public_name = name
-#         self.private_name = '_' + name
-#
-#     def __get__(self, obj, type=None):
-#         value = getattr(obj, self.private_name)
-#         return value * self.units[self.public_name]
-#
-#     def __set__(self, obj, value):
-#         setattr(obj, self.private_name, value)
