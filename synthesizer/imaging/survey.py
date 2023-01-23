46d62368ca558ecc5bd9a5c7f53d13be755591bb
wjr21 <wjr21@sussex.ac.uk>
2023-01-23 13:28:01 +0000

    Added functionality to make a survey and derive image and photometry for it

--- synthesizer/imaging/survey.py
+++ synthesizer/imaging/survey.py
@@ -1,10 +1,12 @@
 """
 Survey functionality 
 """
+import numpy as np
 import synthesizer.exceptions as exceptions
 from synthesizer.imaging import images, spectral_cubes
 from synthesizer.galaxy.particle import ParticleGalaxy
 from synthesizer.galaxy.parametric import ParametricGalaxy
+from synthesizer.utils import m_to_flux, flux_to_luminosity
 
 
 class Instrument:
@@ -20,7 +22,8 @@ class Instrument:
     """
 
     def __init__(self, resolution, filters, psfs=None, depths=None,
-                 aperture=None, resolving_power=None, lam=None):
+                 aperture=None, snrs=None, noises=None,
+                 resolving_power=None, lam=None):
         """
         Initialise the Observatory.
 
@@ -44,7 +47,8 @@ class Instrument:
         # Intilaise noise properties which can be populated by the outside.
         self.aperture = aperture
         self.depths = depths
-        self.noise_sigma = None
+        self.noises = noises
+        self.snrs = snrs
 
     def _check_obs_args(self):
         """
@@ -88,7 +92,7 @@ class Survey:
 
     """
     
-    def __init__(self, galaxies=(), fov=None):
+    def __init__(self, galaxies=(), fov=None, super_resolution_factor=2):
         """
         Initialise the Survey.
 
@@ -102,8 +106,9 @@ class Survey:
         self.nfilters = 0
         self.ngalaxies = 0
 
-        # Information about the field being observered
+        # Information about the field/collection of images being observered
         self.fov = fov
+        self.super_resolution_factor = super_resolution_factor
 
         # Observation configurations are held in a dict, initialise it.
         self.instruments = {}
@@ -113,6 +118,10 @@ class Survey:
 
         # Intialise somewhere to keep survey images, this is populated later
         self.imgs = None
+
+        # Initialise somewhere to keep galaxy photometry. This is the
+        # integrated flux/luminosity in each band in Survey.filters.
+        self.photometry = {}
 
     def _check_survey_args(self):
         """
@@ -127,8 +136,9 @@ class Survey:
         """
         pass
 
-    def add_photometric_instrument(self, filters, resolution, label, psfs=None,
-                                   depths=None, apertures=None):
+    def add_photometric_instrument(self, filters, label, resolution=None,
+                                   psfs=None, depths=None, apertures=None,
+                                   snrs=None, noises=None):
         """
         Adds an instrument and all it's filters to the Survey.
 
@@ -166,11 +176,25 @@ class Survey:
                     " len(filters)=%d, len(apertures)=%d)" % (nfilters,
                                                               len(apertures))
                 )
+        if isinstance(snrs, dict):
+            if nfilters != len(snrs):
+                raise exceptions.InconsistentArguments(
+                    "Inconsistent number of entries in instrument dictionaries"
+                    " len(filters)=%d, len(snrs)=%d)" % (nfilters,
+                                                         len(snrs))
+                )
+        if isinstance(noises, dict):
+            if nfilters != len(noises):
+                raise exceptions.InconsistentArguments(
+                    "Inconsistent number of entries in instrument dictionaries"
+                    " len(filters)=%d, len(noises)=%d)" % (nfilters,
+                                                         len(noises))
+                )
 
         # Create this observation configurations
         self.instruments[label] = Instrument(
             resolution=resolution, filters=filters, psfs=psfs,
-            depths=depths, aperture=apertures
+            depths=depths, aperture=apertures, snrs=snrs, noises=noises
         )
 
         # Record that we included another insturment and count the filters
@@ -221,15 +245,146 @@ class Survey:
         # Count how many galaxies we have
         self.ngalaxies = len(self.galaxies)
 
+    def convert_mag_depth_to_fnu0(self, redshift, cosmo):
+        """
+        Converts depths defined in absolute magnitude to the units of
+        luminosity (erg / s /Hz).
 
-    def get_photometry(self):
+        This is a helpful wrapper to handle the use of different terminology in
+        the SED object.
+
+        Parameters
+        ----------
+        redshift : float
+            The redshift of the observation.
+        cosmo : obj (astropy.cosmology)
+            The cosmology object used to do the cosmological calculations
+            involved in the conversion.
+        """
+        self.convert_mag_depth_to_lnu(redshift, cosmo)
+
+    def convert_mag_depth_to_lnu(self, redshift, cosmo):
+        """
+        Converts depths defined in apparent magnitude to the units of
+        luminosity (erg / s /Hz).
+
+        Parameters
+        ----------
+        redshift : float
+            The redshift of the observation.
+        cosmo : obj (astropy.cosmology)
+            The cosmology object used to do the cosmological calculations
+            involved in the conversion.
+        """
+
+        # Convert the depths, looping over them if we have to.
+        for inst in self.instruments:
+            if isinstance(self.instruments[inst].depths, dict):
+                for key in self.instruments[inst].depths:
+                    flux =  m_to_flux(self.instruments[inst].depths[key])
+                    self.instruments[inst].depths[key] = flux_to_luminosity(
+                        flux, cosmo, redshift
+                    )
+            else:
+                flux =  m_to_flux(self.instruments[inst].depths)
+                self.instruments[inst].depths = flux_to_luminosity(
+                    flux, cosmo, redshift
+                )
+
+    def convert_mag_depth_to_fnu(self):
+        """
+        Converts depths defined in apparent magnitude to the units of
+        flux (nJy).
+        """
+
+        # Convert the depths, looping over them if we have to.
+        for inst in self.instruments:
+            if isinstance(self.instruments[inst].depths, dict):
+                for key in self.instruments[inst].depths:
+                    self.instruments[inst].depths[key] = m_to_flux(
+                        self.instruments[inst].depths[key]
+                    )
+            else:
+                self.instruments[inst].depths = m_to_flux(
+                    self.instruments[inst].depths
+                )
+
+    @staticmethod
+    def _apply_filter_to_spectra(spectra, f):
+        """
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        transmission : float
+             The photometry for this spectra in this band.
+        """
+
+        # Get the mask that removes wavelengths we don't currently care about
+        in_band = f.t > 0
+
+        # Multiply the IFU by the filter transmission curve
+        transmitted = spectra[in_band] * f.t[in_band]
+
+        # Sum over the final axis to "collect" transmission in this filer
+        transmission = np.sum(transmitted)
+
+        return transmission
+            
+    def get_photometry(self, spectra_type, cosmo=None, redshift=None, igm=None):
         """
 
         Parameters
         ----------
         
         """
-        pass
+
+        # We need to handle whether different types of spectra exist.
+        if spectra_type == "intrinsic":
+            pass
+        elif spectra_type == "stellar":
+            pass
+        elif spectra_type == "attenuated":
+            raise exceptions.UnimplementedFunctionality(
+                "Attenuated spectra coming soon!"
+            )
+        else:
+           # TODO: make a UnknownSpectralType error 
+            raise exceptions.InconsistentArguments("Unrecognised spectra_type!")
+
+        # Loop over each instrument
+        for key in self.instruments:
+
+            # Loop over filters in this instrument
+            for f in self.instruments[key].filters:
+
+                # Make an entry in the photometry dictionary for this filter
+                self.photometry[f.filter_code] = np.zeros(self.ngalaxies)
+
+                # Loop over each galaxy
+                for igal in range(self.ngalaxies):
+
+                    if spectra_type not in self.galaxies[igal].spectra:
+                        pass
+
+                    # Are we getting flux or luminosity?
+                    sed = self.galaxies[igal].spectra[spectra_type]
+                    if cosmo is not None:
+                        if sed.fnu is None:
+                            sed.get_fnu(cosmo, redshift, igm)
+                        spectra = sed.fnu
+                    else:
+                        spectra = sed.lnu
+
+                    # Calculate the photometry in this band
+                    phot = self._apply_filter_to_spectra(spectra, f)
+
+                    # Store this photometry
+                    self.photometry[f.filter_code][igal] = phot
+
+        return self.photometry
 
     def make_field_image(self, centre):
         """
@@ -267,16 +422,14 @@ class Survey:
             for gal in self.galaxies:
 
                 # Get images of this galaxy with this instrument
-                img = gal.make_image(inst.resolution, fov=self.fov,
-                                     img_type=img_type,
-                                     sed=gal.spectra_array[spectra_type],
-                                     filters=inst.filters,
-                                     psfs=inst.psfs, depths=inst.depths,
-                                     aperture=inst.aperture,
-                                     kernel_func=kernel_func,
-                                     rest_frame=rest_frame,
-                                     redshift=gal.redshift,
-                                     cosmo=cosmo, igm=igm)
+                img = gal.make_image(
+                    inst.resolution, fov=self.fov, img_type=img_type,
+                    sed=gal.spectra_array[spectra_type], filters=inst.filters,
+                    psfs=inst.psfs, depths=inst.depths,  aperture=inst.aperture,
+                    snrs=inst.snrs, kernel_func=kernel_func,
+                    rest_frame=rest_frame, cosmo=cosmo, igm=igm,
+                    super_resolution_factor=self.super_resolution_factor
+                )
 
                 # Store this result
                 self.imgs[inst].append(img)
