247ef33265ac7db4a6a9b97920017be30049b0d8
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-02-06 11:49:01 +0000

    testing

--- generate_grids/cloudy/convert_cloudy_grid_to_hdf5.py
+++ generate_grids/cloudy/convert_cloudy_grid_to_hdf5.py
@@ -26,6 +26,13 @@ def create_new_grid(grid, synthesizer_data_dir):
     # parse the grid to get the sps model
     sps_grid = grid.split('_cloudy')[0]
 
+    print(sps_grid)
+
+    # testing
+    with h5py.File(f'{path_to_grids}/{sps_grid}.hdf5', 'r') as hf_sps:
+        print('yo')
+        hf_sps.visititems(explore_hdf5_grid)
+
     # open the new grid
     with h5py.File(f'{path_to_grids}/{grid}.hdf5', 'w') as hf:
 
