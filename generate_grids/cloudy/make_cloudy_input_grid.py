5544b50ead78d7a202c08bf7ce7af2ce41228046
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-02-14 15:21:38 +0000

    updated

--- generate_grids/cloudy/make_cloudy_input_grid.py
+++ generate_grids/cloudy/make_cloudy_input_grid.py
@@ -48,22 +48,107 @@ def load_cloudy_parameters(param_file='default.yaml', default_param_file='defaul
         except yaml.YAMLError as exc:
             print(exc)
 
-    # find any differences - this breaks when a list is defined
-    cloudy_params_set = set(cloudy_params.items())
-    default_cloudy_params_set = set(default_cloudy_params.items())
+    # copy over defaults where the parameter is not already set
+    for k, v in default_cloudy_params.items():
+        if k not in cloudy_params.keys():
+            cloudy_params[k] = v
 
-    param_changes = dict(cloudy_params_set - default_cloudy_params_set)
+    # first loop through and identify changes that are not in lists (e.g. U_model) and add them to the model name
+    out_str_ = f"_cloudy-{cloudy_params['cloudy_version']}"
 
-    # update any custom parameters
-    for k, v in kwarg_parameters.items():
-        cloudy_params[k] = v
-        param_changes[k] = v
+    # flag denoting that the parameter file includes at least one list. Used below.
+    parameter_list = False
+    for k, v in cloudy_params.items():
+        if type(v) is not list:
+            if v != default_cloudy_params[k]:
+                out_str_ += f'-{k}{str(v).replace("-", "m")}'
+        else:
+            parameter_list = True
 
-    out_str = ''
-    for k, v in param_changes.items():
-        out_str += f'-{k}{str(v).replace("-", "m")}'
+    # search for any lists of parameters.
+    # currently exits once it finds the *first* list
+    # TODO: adapt to accept multiple varied parameters
 
-    return [cloudy_params], [out_str]
+    if not parameter_list:
+
+        output_cloudy_params = [cloudy_params]
+        output_cloudy_names = [out_str_]
+
+    else:
+
+        output_cloudy_params = []
+        output_cloudy_names = []
+
+        for k, v in cloudy_params.items():
+            if type(v) is list:
+                for _v in v:
+
+                    cloudy_params_ = deepcopy(cloudy_params)
+
+                    # update the value in our default dictionary
+                    cloudy_params_[k] = _v
+
+                    # save to list of cloudy param dicts
+                    output_cloudy_params.append(cloudy_params_)
+
+                    # replace negative '-' with m
+                    # out_str = f'-{k}{str(_v).replace("-", "m")}'
+                    out_str = f'-{k}{str(_v).replace("-", "m")}'
+
+                    # save to list of output strings
+                    output_cloudy_names.append(out_str_+out_str)
+
+    return output_cloudy_params, output_cloudy_names
+
+
+# def load_cloudy_parameters(param_file='default.yaml', default_param_file='default.yaml',
+#                            **kwarg_parameters):
+#     """
+#     Load CLOUDY parameters from a YAML file
+#
+#     Parameters
+#     ----------
+#     param_file : str
+#         Location of YAML file.
+#     **kwarg_parameters
+#         Additional parameters you may wish to amend manually
+#
+#     Returns
+#     -------
+#     bool
+#         True if successful, False otherwise.
+#     """
+#
+#     # open paramter file
+#     with open(param_file, "r") as stream:
+#         try:
+#             cloudy_params = yaml.safe_load(stream)
+#         except yaml.YAMLError as exc:
+#             print(exc)
+#
+#     # open default parameter file
+#     with open(default_param_file, "r") as stream:
+#         try:
+#             default_cloudy_params = yaml.safe_load(stream)
+#         except yaml.YAMLError as exc:
+#             print(exc)
+#
+#     # find any differences - this breaks when a list is defined
+#     cloudy_params_set = set(cloudy_params.items())
+#     default_cloudy_params_set = set(default_cloudy_params.items())
+#
+#     param_changes = dict(cloudy_params_set - default_cloudy_params_set)
+#
+#     # update any custom parameters
+#     for k, v in kwarg_parameters.items():
+#         cloudy_params[k] = v
+#         param_changes[k] = v
+#
+#     out_str = ''
+#     for k, v in param_changes.items():
+#         out_str += f'-{k}{str(v).replace("-", "m")}'
+#
+#     return [cloudy_params], [out_str]
 
 
 def make_directories(synthesizer_data_dir, sps_grid, cloudy_name):
@@ -194,21 +279,18 @@ if __name__ == "__main__":
                                                   'and submission script for '
                                                   'a given SPS grid.'))
 
-    parser.add_argument("-dir", "--directory", type=str, required=True)
+    parser.add_argument("-dir", "--directory", type=str, required=False, default=None)
 
-    parser.add_argument("-m", "--machine", type=str,
-                        choices=['cosma7', 'apollo'], default=None,
-                        help=('Write a submission script for the '
-                              'specified machine. Default is None - '
-                              'write no submission script.'))
+    parser.add_argument("-m", "--machine", type=str, choices=['cosma7', 'apollo'], default=None, help=(
+        'Write a submission script for the ', 'specified machine. Default is None - ', 'write no submission script.'))
 
     parser.add_argument("-sps", "--sps_grid", type=str,
-                        nargs='+', required=True,
+                        nargs='+', required=False, default=None,
                         help=('The SPS grid(s) to run the cloudy grid on. '
                               'Multiple grids can be listed as: \n '
                               '  --sps_grid grid_1 grid_2'))
 
-    parser.add_argument("-p", "--params", type=str, required=True,
+    parser.add_argument("-p", "--params", type=str, required=False, default='default.yaml',
                         help='YAML parameter file of cloudy parameters')
 
     parser.add_argument("-c", "--cloudy", type=str, nargs='?',
@@ -219,39 +301,42 @@ if __name__ == "__main__":
     synthesizer_data_dir = args.directory
     cloudy = args.cloudy
 
-    for sps_grid in args.sps_grid:
+    print(f"Loading the cloudy parameters from: {args.params}")
 
-        print(f"Loading the SPS grid: {sps_grid}")
+    # load the cloudy parameters you are going to run
+    c_params, c_name = load_cloudy_parameters(args.params)
 
-        # load the specified SPS grid
-        grid = Grid(sps_grid, grid_dir=f'{synthesizer_data_dir}/grids')
+    for i, cloudy_name in enumerate(c_name):
+        print(i, cloudy_name)
 
-        print(f"Loading the cloudy parameters from: {args.params}")
+    if args.sps_grid:
 
-        # load the cloudy parameters you are going to run
-        c_params, c_name = load_cloudy_parameters(args.params)
+        for sps_grid in args.sps_grid:
 
-        for i, (cloudy_params, cloudy_name) in \
-                enumerate(zip(c_params, c_name)):
+            print(f"Loading the SPS grid: {sps_grid}")
 
-            cloudy_name = 'cloudy'+cloudy_name
+            # load the specified SPS grid
+            grid = Grid(sps_grid, grid_dir=f'{synthesizer_data_dir}/grids')
 
-            output_dir = make_directories(synthesizer_data_dir, sps_grid,
-                                          cloudy_name)
+            for i, (cloudy_params, cloudy_name) in \
+                    enumerate(zip(c_params, c_name)):
 
-            print((f"Generating cloudy grid for ({i}) "
-                   f"{cloudy_name} in {output_dir}"))
+                output_dir = make_directories(synthesizer_data_dir, sps_grid,
+                                              cloudy_name)
 
-            N = make_cloudy_input_grid(output_dir, grid, cloudy_params)
+                print((f"Generating cloudy grid for ({i}) "
+                       f"{cloudy_name} in {output_dir}"))
 
-            if args.machine == 'apollo':
-                apollo_submission_script(N, output_dir, cloudy)
-            elif args.machine == 'cosma7':
-                cosma7_submission_script(N, output_dir, cloudy,
-                                         cosma_project='cosma7',
-                                         cosma_account='dp004')
-            elif args.machine is None:
-                print(("No machine specified. Skipping "
-                       "submission script write out"))
-            else:
-                ValueError(f'Machine {args.machine} not recognised.')
+                N = make_cloudy_input_grid(output_dir, grid, cloudy_params)
+
+                if args.machine == 'apollo':
+                    apollo_submission_script(N, output_dir, cloudy)
+                elif args.machine == 'cosma7':
+                    cosma7_submission_script(N, output_dir, cloudy,
+                                             cosma_project='cosma7',
+                                             cosma_account='dp004')
+                elif args.machine is None:
+                    print(("No machine specified. Skipping "
+                           "submission script write out"))
+                else:
+                    ValueError(f'Machine {args.machine} not recognised.')
