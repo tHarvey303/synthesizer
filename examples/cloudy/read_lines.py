673c7c97b6a6af902906bbc6e1e21130af97ed9c
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-06-19 13:12:06 +0100

    add more comments

--- examples/cloudy/read_lines.py
+++ examples/cloudy/read_lines.py
@@ -1,31 +1,32 @@
-
+"""
+Read in a full emission line grid from cloudy and produce a linelist of strong lines relative to another line.
+"""
 
 import numpy as np
 from synthesizer.cloudy import read_lines
 
 
-synthesizer_data_dir = '/Users/stephenwilkins/Dropbox/Research/data/synthesizer/cloudy/'
-model = 'bpass-2.2.1-bin_chabrier03-0.1,100.0_cloudy'
-filename = '0_7'
-threshold_line = 'H 1 4862.69A'
-relative_threshold = 4.0
+output_dir = 'output'
+model_name = 'default'
+
+threshold_line = 'H 1 4862.69A' # Hbeta
+relative_threshold = 2.5 # log
 
 cloudy_ids, blends, wavelengths, intrinsic, emergent = read_lines(
-    f'{synthesizer_data_dir}/{model}/{filename}')
+    f'{output_dir}/{model_name}')
 
 threshold = emergent[cloudy_ids == threshold_line] - relative_threshold
 
+# select line meeting various conditions
 s = (emergent > threshold) & (blends == False) & (wavelengths < 50000)
 
-print(cloudy_ids[s])
-print(len(cloudy_ids[s]))
-print(len(set(cloudy_ids[s])))
 
-ion = np.array([''.join(id.split(' ')[:2]) for id in cloudy_ids])
-print(ion[s])
+print(f'number of lines: {len(cloudy_ids[s])}')
 
-# with open('all_lines.dat', 'w') as f:
-#     f.writelines('\n'.join(cloudy_ids) + '\n')
-#
-# with open('default_lines.dat', 'w') as f:
-#     f.writelines('\n'.join(cloudy_ids[s]) + '\n')
+# save a list of all lines
+with open('alllines.dat', 'w') as f:
+    f.writelines('\n'.join(cloudy_ids) + '\n')
+
+# save a list of only lines meeting the conditions above
+with open('defaultlines.dat', 'w') as f:
+    f.writelines('\n'.join(cloudy_ids[s]) + '\n')
