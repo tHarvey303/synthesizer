3180859de37ef9a1f2a5093711fce26794970ab8
wjr21 <wjr21@sussex.ac.uk>
2023-06-19 19:15:09 +0100

    Some initial clean up of the Morphology class

--- synthesizer/parametric/morphology.py
+++ synthesizer/parametric/morphology.py
@@ -3,6 +3,7 @@ from astropy.modeling.models import Sersic2D as Sersic2D_
 from unyt import kpc, mas
 from unyt.dimensions import length, angle
 import matplotlib.pyplot as plt
+import synthesizer.exceptions
 
 
 class MorphologyBase:
@@ -12,11 +13,11 @@ class MorphologyBase:
 
     Methods
     -------
-    plot
+    plot_density_grid
         shows a plot of the model for a given resolution and npix
     """
 
-    def plot(self, resolution, npix=None, cosmo=None, z=None):
+    def plot_density_grid(self, resolution, npix=None, cosmo=None, z=None):
         """
         Produce a plot of the current morphology
 
@@ -30,7 +31,7 @@ class MorphologyBase:
 
         xx, yy = np.meshgrid(bins, bins)
 
-        img = self.img(xx, yy)
+        img = self.compute_density_grid(xx, yy)
 
         plt.figure()
         plt.imshow(np.log10(img), origin='lower', interpolation='nearest',
@@ -41,27 +42,30 @@ class MorphologyBase:
 class Sersic2D(MorphologyBase):
 
     """
-    A class holding the Sersic2D profile. This is a wrapper around the astropy.models.Sersic2D class.
-
+    A class holding the Sersic2D profile. This is a wrapper around the
+    astropy.models.Sersic2D class.
 
     Methods
     -------
-    img
-        returns an image
+    compute_density_grid
+        Calculates and returns the density grid defined by this Morphology
     """
 
-    def __init__(self, p, cosmo=None, z=None):
+    def __init__(self, params, cosmo=None, z=None):
 
-        self.update(p, cosmo=cosmo, z=z)
-
-    def update(self, p, cosmo=None, z=None):
-
+        # Define the initial parameter set
         self.parameters = {
             'r_eff_kpc': None,
             'r_eff_mas': None,
             'n': 1,
             'ellip': 0,
-            'theta': 0}
+            'theta': 0,
+        }
+
+        # Initialise the Morphology object based on the 
+        self.update(params, cosmo=cosmo, z=z)
+
+    def update(self, p, cosmo=None, z=None):
 
         for key, value in list(p.items()):
             self.parameters[key] = value
@@ -71,7 +75,7 @@ class Sersic2D(MorphologyBase):
         elif p['r_eff'].units.dimensions == length:
             self.parameters['r_eff_kpc'] = p['r_eff'].to('kpc').value
 
-        # if cosmology and redshift provided calculate the conversion of pkpc to mas
+        # If cosmology and redshift provided calculate the conversion of pkpc to mas
         if cosmo and z:
             self.kpc_proper_per_mas = cosmo.kpc_proper_per_arcmin(z).to('kpc/mas').value
 
@@ -83,44 +87,53 @@ class Sersic2D(MorphologyBase):
                     self.kpc_proper_per_mas
 
         if self.parameters['r_eff_kpc']:
-            self.model_kpc = Sersic2D_(amplitude=1, r_eff=self.parameters['r_eff_kpc'],
-                                       n=self.parameters['n'], ellip=self.parameters['ellip'], theta=self.parameters['theta'])
+            self.model_kpc = Sersic2D_(
+                amplitude=1, r_eff=self.parameters['r_eff_kpc'],
+                n=self.parameters['n'], ellip=self.parameters['ellip'],
+                theta=self.parameters['theta']
+            )
         else:
             self.model_kpc = None
 
         if self.parameters['r_eff_mas']:
-            self.model_mas = Sersic2D_(amplitude=1, r_eff=self.parameters['r_eff_mas'],
-                                       n=self.parameters['n'], ellip=self.parameters['ellip'], theta=self.parameters['theta'])
+            self.model_mas = Sersic2D_(
+                amplitude=1, r_eff=self.parameters['r_eff_mas'],
+                n=self.parameters['n'], ellip=self.parameters['ellip'],
+                theta=self.parameters['theta']
+            )
         else:
             self.model_mas = None
 
-    def img(self, xx, yy, units=kpc):
+    def compute_density_grid(self, xx, yy, units=kpc):
         """
-        Produce a plot of the current morphology
+        Compute the density grid defined by this morphology as a function of
+        the input coordinate grids.
+
+        This acts as a wrapper to astropy functionality (defined above) which
+        only work in units of kpc or milliarcseconds (mas)
 
         Parameters
         ----------
-        xx: float array
-            x values on 2D grid
-        yy: float array
-            y values on 2D grid
-
+        xx: array-like (float)
+            x values on a 2D grid.
+        yy: array-like (float)
+            y values on a 2D grid.
+        units : unyt.unit
+            The units in which the coordinate grids are defined.
 
         Returns
         ----------
-        np.ndarray
-            image
-
-        Example
-        ----------
-
-        >>> bins = resolution * np.arange(-npix/2, npix/2)
-        >>> xx, yy = np.meshgrid(bins, bins)
-        >>> img = self.img(xx, yy)
-
+        density_grid : np.ndarray
+            The density grid produced
         """
 
+        # Call the appropriate model function
         if units == kpc:
             return self.model_kpc(xx, yy)
-        if units == mas:
+        elif units == mas:
             return self.model_mas(xx, yy)
+        else:
+            raise exceptions.InconsistentArguments(
+                "Only kpc and milliarcsecond (mas) units are supported "
+                "for morphologies."
+            )
