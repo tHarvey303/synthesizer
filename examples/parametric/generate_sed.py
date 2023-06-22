9b51c28d644d19fbb6cecc469114bfb89f0355b7
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-06-22 07:54:10 +0100

    added generate_sed example

--- examples/parametric/generate_sed.py
+++ examples/parametric/generate_sed.py
@@ -6,13 +6,11 @@ photometry. This example will:
 """
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 
 from synthesizer.filters import FilterCollection
 from synthesizer.grid import Grid
 from synthesizer.parametric.sfzh import SFH, ZH, generate_sfzh
 from synthesizer.parametric.galaxy import Galaxy
-from synthesizer.plt import single, single_histxy, mlabel
 from unyt import yr, Myr
 
 
@@ -27,57 +25,57 @@ if __name__ == '__main__':
     grid_dir = script_path + "/../../tests/test_grid/"
     grid = Grid(grid_name, grid_dir=grid_dir)
 
-    # --- define the parameters of the star formation and metal enrichment histories
+    # define the parameters of the star formation and metal enrichment histories
     sfh_p = {'duration': 10 * Myr}
     Z_p = {'log10Z': -2.0}  # can also use linear metallicity e.g. {'Z': 0.01}
     stellar_mass = 1E8
 
-    # --- define the functional form of the star formation and metal enrichment histories
+    # define the functional form of the star formation and metal enrichment histories
     sfh = SFH.Constant(sfh_p)  # constant star formation
     print(sfh)  # print sfh summary
 
     Zh = ZH.deltaConstant(Z_p)  # constant metallicity
 
-    # --- get the 2D star formation and metal enrichment history for the given SPS grid. This is (age, Z).
-    sfzh = generate_sfzh(grid.log10ages, grid.metallicities, sfh, Zh, stellar_mass=stellar_mass)
+    # get the 2D star formation and metal enrichment history for the given SPS grid. 
+    sfzh = generate_sfzh(grid.log10age, grid.metallicity, sfh, Zh, stellar_mass=stellar_mass)
 
-    # --- create a galaxy object
+    # create a galaxy object
     galaxy = Galaxy(sfzh)
 
-    # # --- generate pure stellar spectra alone
+    # generate pure stellar spectra alone
     galaxy.get_spectra_stellar(grid)
     print("Pure stellar spectra")
-    galaxy.plot_spectra()
+    galaxy.plot_spectra(show=True)
 
-    # # --- generate intrinsic spectra (which includes reprocessing by gas)
+    # generate intrinsic spectra (which includes reprocessing by gas)
     galaxy.get_spectra_intrinsic(grid, fesc = 0.5)
     print("Intrinsic spectra")
-    galaxy.plot_spectra()
+    galaxy.plot_spectra(show=True)
 
-    # # --- simple dust and gas screen
+    # simple dust and gas screen
     galaxy.get_spectra_screen(grid, tauV = 0.1, fesc = 0.5)
     print("Simple dust and gas screen")
-    galaxy.plot_spectra()
+    galaxy.plot_spectra(show=True)
 
-    # --- CF00 model
+    # CF00 model
     galaxy.get_spectra_CharlotFall(grid, tauV_ISM=0.1, tauV_BC=0.1, alpha_ISM=-0.7, alpha_BC=-1.3)
     print("CF00 model")
-    galaxy.plot_spectra()
+    galaxy.plot_spectra(show=True)
 
-    # # --- pacman model
+    # pacman model
     galaxy.get_spectra_pacman(grid, tauV = 0.1, fesc = 0.5)
     print("Pacman model")
-    galaxy.plot_spectra()
+    galaxy.plot_spectra(show=True)
 
-    # # --- pacman model (no Lyman-alpha escape and no dust)
+    # pacman model (no Lyman-alpha escape and no dust)
     galaxy.get_spectra_pacman(grid, fesc = 0.0, fesc_LyA = 0.0)
     print("Pacman model (no Ly-alpha escape, and no dust)")
-    galaxy.plot_spectra()
+    galaxy.plot_spectra(show=True)
 
-    # # --- pacman model (complex)
+    # pacman model (complex)
     galaxy.get_spectra_pacman(grid, fesc=0.0, fesc_LyA=0.5, tauV=0.6)
     print("Pacman model (complex)")
-    galaxy.plot_spectra()
+    galaxy.plot_spectra(show=True)
 
     # --- CF00 model implemented within pacman model
     galaxy.get_spectra_pacman(grid, fesc = 0.1, fesc_LyA = 0.1, tauV=[1.,1.], alpha = [-1,-1], CF00=True)
