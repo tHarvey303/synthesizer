3e6eb170f0a2eef6377df6ffdeae27800949c9c7
wjr21 <wjr21@sussex.ac.uk>
2023-08-02 20:18:03 +0100

    Dust to metal ratio can now be an array

--- synthesizer/extensions/los.c
+++ synthesizer/extensions/los.c
@@ -22,15 +22,16 @@ particles.
  *
  * @param 
  */
-PyObject *compute_metal_surface_dens(PyObject *self, PyObject *args) {
+PyObject *compute_dust_surface_dens(PyObject *self, PyObject *args) {
 
   const int nstar, ngas, kdim;
+  const double threshold;
   const PyArrayObject *np_kernel, *np_star_pos, *np_gas_pos, *np_gas_sml;
-  const PyArrayObject *np_gas_met, *np_gas_mass;
+  const PyArrayObject *np_gas_met, *np_gas_mass, *np_gas_dtm;
 
-  if(!PyArg_ParseTuple(args, "OOOOOOiii", &np_kernel, &np_star_pos,
+  if(!PyArg_ParseTuple(args, "OOOOOOOiiid", &np_kernel, &np_star_pos,
                        np_gas_pos, &np_gas_sml, &np_gas_met, &np_gas_mass,
-                       &nstar, &ngas, &kdim))
+                       &np_gas_dtm, &nstar, &ngas, &kdim, &threshold))
     return NULL;
 
   /* Quick check to make sure our inputs are valid. */
@@ -39,8 +40,8 @@ PyObject *compute_metal_surface_dens(PyObject *self, PyObject *args) {
   if (kdim == 0) return NULL;
 
   /* Set up arrays to hold the surface densities themselves. */
-  double *los_metsds = malloc(nstar * sizeof(double));
-  bzero(los_metsds, nstar * sizeof(double));
+  double *los_dustsds = malloc(nstar * sizeof(double));
+  bzero(los_dustsds, nstar * sizeof(double));
 
   /* Extract a pointers to the actual data in the numpy arrays. */
   const double *kernel = PyArray_DATA(np_kernel);
@@ -49,6 +50,7 @@ PyObject *compute_metal_surface_dens(PyObject *self, PyObject *args) {
   const double *gas_sml = PyArray_DATA(np_gas_sml);
   const double *gas_met = PyArray_DATA(np_gas_met);
   const double *gas_mass = PyArray_DATA(np_gas_mass);
+  const double *gas_dtm = PyArray_DATA(np_gas_dtm);
 
   /* Loop over stars */
   for (int istar = 0; istar < nstar; istar++) {
@@ -63,7 +65,9 @@ PyObject *compute_metal_surface_dens(PyObject *self, PyObject *args) {
       double y = gas_pos[igas * 3 + 1] - star_pos[istar * 3 + 1];
 
       /* Early skip if the star doesn't fall in the gas particles kernel. */
-      if (abs(x) > gas_sml[igas] || abs(y) > gas_sml[igas]) continue;
+      if (abs(x) > threshold * gas_sml[igas] ||
+          abs(y) > threshold * gas_sml[igas])
+        continue;
 
       /* Convert separation to distance. */
       double rsqu = x * x + y * y;
@@ -73,30 +77,31 @@ PyObject *compute_metal_surface_dens(PyObject *self, PyObject *args) {
       double q = rsqu / sml_squ;
 
       /* Skip gas particles outside the kernel. */
-      if (q > 1) continue;
+      if (q > threshold) continue;
 
       /* Get the value of the kernel at q. */
       int index = kdim * q;
       double kvalue = kernel[index];
 
       /* Finally, compute the metal surface density itself. */
-      los_metsds[istar] += gas_mass[igas] * gas_met[igas] / sml_squ * kvalue;
+      los_dustsds[istar] += gas_dtm[igas] * gas_mass[igas] *
+        gas_met[igas] / sml_squ * kvalue;
       
     }
   }
 
   /* Reconstruct the python array to return. */
   npy_intp np_dims[1] = {nstar,};
-  PyArrayObject *out_los_metsds =
+  PyArrayObject *out_los_dustsds =
     (PyArrayObject *) PyArray_SimpleNewFromData(1, np_dims, NPY_FLOAT64,
-                                                los_metsds);
+                                                los_dustsds);
 
   return Py_BuildValue("N", out_los_metsds);
 }
 
 /* Below is all the gubbins needed to make the module importable in Python. */
 static PyMethodDef LosMethods[] = {
-  {"compute_metal_surface_dens", compute_metal_surface_dens, METH_VARARGS,
+  {"compute_dust_surface_dens", compute_metal_surface_dens, METH_VARARGS,
    "Method for calculating line of sight metal surface densities."},
   {NULL, NULL, 0, NULL} 
 };
@@ -104,7 +109,7 @@ static PyMethodDef LosMethods[] = {
 /* Make this importable. */
 static struct PyModuleDef moduledef = {
         PyModuleDef_HEAD_INIT,
-        "los_met_surface_dens",                                /* m_name */
+        "los_dust_surface_dens",                                /* m_name */
         "A module to calculate los metal surface densities",   /* m_doc */
         -1,                                                    /* m_size */
         LosMethods,                                            /* m_methods */
