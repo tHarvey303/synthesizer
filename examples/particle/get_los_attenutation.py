807711c04cdccffa2c1d1ceb2144fcaf705bbd4c
wjr21 <wjr21@sussex.ac.uk>
2023-08-04 16:06:02 +0100

    Temporary example which makes timging plots comparing the loop to the tree

--- examples/particle/get_los_attenutation.py
+++ examples/particle/get_los_attenutation.py
@@ -48,64 +48,123 @@ sfh = SFH.Constant(sfh_p)  # constant star formation
 mass = 10**10
 sfzh = generate_sfzh(log10ages, metallicities, sfh, Zh, stellar_mass=mass)
 
-for n, ngas in zip([10, 100, 1000] * 3,
-                   [100, 100, 100,
-                    1000, 1000, 1000,
-                    10000, 10000, 10000]):
+for n in [10, 100, 1000, 10000]:
+    xs = []
+    loop_ys = []
+    tree_ys = []
+    precision = []
+    for ngas in np.logspace(np.log10(n), 6, 20, dtype=int):
 
-    # Make a fake galaxy
+        # Make a fake galaxy
 
-    # First make the stars
+        # First make the stars
 
-    # Generate some random coordinates
-    coords = CoordinateGenerator.generate_3D_gaussian(n)
+        # Generate some random coordinates
+        coords = CoordinateGenerator.generate_3D_gaussian(n)
 
-    # Calculate the smoothing lengths from radii
-    cent = np.mean(coords, axis=0)
-    rs = np.sqrt(
-        (coords[:, 0] - cent[0]) ** 2
-        + (coords[:, 1] - cent[1]) ** 2
-        + (coords[:, 2] - cent[2]) ** 2
-    )
-    rs[rs < 0.2] = 0.6  # Set a lower bound on the "smoothing length"
+        # Calculate the smoothing lengths from radii
+        cent = np.mean(coords, axis=0)
+        rs = np.sqrt(
+            (coords[:, 0] - cent[0]) ** 2
+            + (coords[:, 1] - cent[1]) ** 2
+            + (coords[:, 2] - cent[2]) ** 2
+        )
+        rs[rs < 0.2] = 0.6  # Set a lower bound on the "smoothing length"
 
-    # Sample the SFZH, producing a Stars object
-    # we will also pass some keyword arguments for attributes
-    # we will need for imaging
-    stars = sample_sfhz(sfzh, n, coordinates=coords, 
-                        current_masses=np.full(n, 10**8.7 / n), 
-                        smoothing_lengths=rs / 2, redshift=1)
+        # Sample the SFZH, producing a Stars object
+        # we will also pass some keyword arguments for attributes
+        # we will need for imaging
+        stars = sample_sfhz(sfzh, n, coordinates=coords, 
+                            current_masses=np.full(n, 10**8.7 / n), 
+                            smoothing_lengths=rs / 2, redshift=1)
 
-    # Now make the gas
+        # Now make the gas
 
-    # Generate some random coordinates
-    coords = CoordinateGenerator.generate_3D_gaussian(ngas)
+        # Generate some random coordinates
+        coords = CoordinateGenerator.generate_3D_gaussian(ngas)
 
-    # Calculate the smoothing lengths from radii
-    cent = np.mean(coords, axis=0)
-    rs = np.sqrt(
-        (coords[:, 0] - cent[0]) ** 2
-        + (coords[:, 1] - cent[1]) ** 2
-        + (coords[:, 2] - cent[2]) ** 2
-    )
-    rs[rs < 0.2] = 0.6  # Set a lower bound on the "smoothing length"
+        # Calculate the smoothing lengths from radii
+        cent = np.mean(coords, axis=0)
+        rs = np.sqrt(
+            (coords[:, 0] - cent[0]) ** 2
+            + (coords[:, 1] - cent[1]) ** 2
+            + (coords[:, 2] - cent[2]) ** 2
+        )
+        rs[rs < 0.2] = 0.6  # Set a lower bound on the "smoothing length"
 
-    gas = Gas(masses=np.random.uniform(10**6, 10**6.5, ngas),
-              metallicities=np.random.uniform(0.01, 0.05, ngas),
-              coordinates=coords,
-              smoothing_lengths=rs / 4,
-              dust_to_metal_ratio=0.2)
+        gas = Gas(masses=np.random.uniform(10**6, 10**6.5, ngas),
+                  metallicities=np.random.uniform(0.01, 0.05, ngas),
+                  coordinates=coords,
+                  smoothing_lengths=rs / 4,
+                  dust_to_metal_ratio=0.2)
 
-    # Create galaxy object
-    galaxy = Galaxy("Galaxy", stars=stars, gas=gas, redshift=1)
+        # Create galaxy object
+        galaxy = Galaxy("Galaxy", stars=stars, gas=gas, redshift=1)
 
-    # Create a fake kernel
-    kernel = np.random.normal(0.5, 0.25, 100)
-    kernel = np.sort(kernel)[::-1]
-    kernel /= np.sum(kernel)
+        # Create a fake kernel
+        kernel = np.random.normal(0.5, 0.25, 100)
+        kernel = np.sort(kernel)[::-1]
+        kernel /= np.sum(kernel)
 
-    # Calculate the tau_vs
-    start = time.time()
-    tau_v = galaxy.calculate_los_tau_v(kappa=0.5, kernel=kernel)
-    print("Calculation took %.2f seconds for nstar=%d and ngas=%d" % (
-        time.time() - start, n, ngas))
+        # Calculate the tau_vs
+        start = time.time()
+        tau_v = galaxy.calculate_los_tau_v(kappa=0.07, kernel=kernel, force_loop=1)
+        loop_time = time.time() - start
+        loop_sum = np.sum(tau_v)
+
+        # Calculate the tau_vs
+        start = time.time()
+        tau_v = galaxy.calculate_los_tau_v(kappa=0.07, kernel=kernel)
+        tree_time = time.time() - start
+        tree_sum = np.sum(tau_v)
+
+        xs.append(n * ngas)
+        loop_ys.append(loop_time)
+        tree_ys.append(tree_time)
+        precision.append(np.abs(tree_sum - loop_sum) / loop_sum * 100)
+
+        print("LOS calculation with tree took %.4f seconds for nstar=%d and ngas=%d" % (
+            tree_time, n, ngas))
+        print("LOS calculation with loop took %.4f seconds for nstar=%d and ngas=%d" % (
+            loop_time, n, ngas))
+        print("Ratio in wallclock: Time_loop/Time_tree=%.4f" % (loop_time / tree_time))
+        print("Tree gave=%.2f Loop gave=%.2f Normalised residual=%.4f%%" % (
+            tree_sum, loop_sum, np.abs(tree_sum - loop_sum) / loop_sum * 100
+        )
+              )
+
+    xs = np.array(xs)
+    sinds = np.argsort(xs)
+    xs = xs[sinds]
+    loop_ys = np.array(loop_ys)[sinds]
+    tree_ys = np.array(tree_ys)[sinds]
+    precision = np.array(precision)[sinds]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.loglog()
+    ax.grid()
+
+    ax.plot(xs, loop_ys, label="Loop")
+    ax.plot(xs, tree_ys, label="Tree")
+
+    ax.set_ylabel("Wallclock (s)")
+    ax.set_xlabel("$N_\star N_\mathrm{gas}$")
+
+    ax.legend()
+
+    fig.savefig("../los_timing_nstar%d.png" % n, dpi=100, bbox_inches="tight")
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.semilogx()
+    ax.grid()
+
+    ax.plot(xs, precision, label="Loop/Tree")
+
+    ax.set_ylabel(r"$|\tau_{V, tree} - \tau_{V, loop}| / \tau_{V, loop}$ (%)")
+    ax.set_xlabel("$N_\star N_\mathrm{gas}$")
+
+    ax.legend()
+
+    fig.savefig("../los_precision_nstar%d.png" % n, dpi=100, bbox_inches="tight")
