f3f426dbcfa8460bbcd9783dc3f43ecb1ade844e
wjr21 <wjr21@sussex.ac.uk>
2023-08-25 14:26:17 +0100

    Moved Feltre16 to generate_grids module

--- synthesizer/particle/blackholes.py
+++ synthesizer/particle/blackholes.py
@@ -58,7 +58,7 @@ class BlackHoles(Particles):
     bol_luminosity = Quantity()
     bb_temperture = Quantity()
 
-    def __init__(self, masses, metallicities, redshift=None,
+    def __init__(self, masses, metallicities=None, redshift=None,
                  accretion_rate=None, coordinates=None,
                  velocities=None, softening_length=None):
         """
@@ -157,7 +157,7 @@ class BlackHoles(Particles):
 
         """
 
-        self.bol_luminosity = epsilon * self.accretion_rate * c**2
+        self.bol_luminosity = epsilon * self.accretion_rate * c ** 2
 
         return self.bol_luminosity
 
@@ -172,68 +172,3 @@ class BlackHoles(Particles):
 #         return None
 
 
-# class Feltre16:
-
-#     """
-#     A class to hold routines for employing the Feltre16 AGN model.
-#     """
-
-#     def __init__(self):
-#         return None
-
-#     def incident(self, lam, alpha, luminosity=1):
-#         """
-#         Create intrinsic narrow-line AGN spectra as utilised by Feltre et al. (2016). This is utilised to build the cloudy grid.
-
-#         Parameters
-#         ----------
-#         lam : array
-#             Wavelength grid (array) in angstrom or unyt
-
-#         alpha: float
-#             UV/optical power-law index. Expected to be -2.0<alpha<-1.2
-
-#         luminosity: float
-#             Bolometric luminosity. Set to unity.
-
-
-#         Returns
-#         -------
-#         lnu
-#             Spectral luminosity density.
-#         """
-
-#         # create empty luminosity array
-#         lnu = np.zeros(lam.shape)
-
-#         # calculate frequency
-#         nu = c/lam
-
-#         # define edges
-#         edges = [10., 2500., 100000., 1000000.] * Angstrom  # Angstrom
-
-#         # define indices
-#         indices = [alpha, -0.5, 2.]
-
-#         # define normalisations
-#         norms = [1.]
-
-#         # calcualte remaining normalisations
-#         for i, (edge_lam, ind1, ind2) in enumerate(zip(edges[1:], indices, indices[1:])):
-
-#             edge_nu = c/edge_lam
-
-#             norm = norms[i]*(edge_nu**ind1)/(edge_nu**ind2)
-#             norms.append(norm)
-
-#         # now construct spectra
-#         for e1, e2, ind, norm in zip(edges[0:], edges[1:], indices, norms):
-
-#             # identify indices within the wavelength range
-#             s = (lam >= e1) & (lam < e2)
-
-#             lnu[s] = norm * nu[s]**ind
-
-#         # normalise -- not yet implemented
-
-#         return lnu
