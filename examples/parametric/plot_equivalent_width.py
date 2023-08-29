464411c874dabbc4826e674f313b1e110d7f75b2
christopherlovell <chrisl1991@hotmail.co.uk>
2023-08-29 13:05:57 +0100

    equivalent width example moved

--- examples/parametric/plot_equivalent_width.py
+++ examples/parametric/plot_equivalent_width.py
@@ -1,4 +1,7 @@
 """
+Plot equivalent width for UV indices
+====================================
+
 Example for generating the equivalent width for a set of UV indices from a parametric galaxy
 - build a parametric galaxy (see make_sfzh)
 - calculate equivalent width (see sed.py)
@@ -9,15 +12,15 @@ import matplotlib.pyplot as plt
 from synthesizer.grid import Grid
 from synthesizer.parametric.sfzh import SFH, ZH, generate_sfzh
 from synthesizer.parametric.galaxy import Galaxy as Galaxy
-from synthesizer.sed import uv_indices as index
+from synthesizer.sed import uv_indices
 from unyt import yr, Myr
 
 
-def get_ew(index, Z, imf, grid, EqW, mode):
+def get_ew(index, Z, smass, grid, EqW, mode):
     sfh_p = {'duration': 100 * Myr}
 
     Z_p = {'Z': Z}  # can also use linear metallicity e.g. {'Z': 0.01}
-    stellar_mass = imf
+    stellar_mass = smass
 
     # --- define the functional form of the star formation and metal enrichment histories
     sfh = SFH.Constant(sfh_p)  # constant star formation
@@ -26,7 +29,7 @@ def get_ew(index, Z, imf, grid, EqW, mode):
     Zh = ZH.deltaConstant(Z_p)  # constant metallicity
 
     # --- get 2D star formation and metal enrichment history for the given SPS grid. This is (age, Z).
-    sfzh = generate_sfzh(grid.log10ages, grid.metallicities, sfh, Zh, stellar_mass=stellar_mass)
+    sfzh = generate_sfzh(grid.log10age, grid.metallicity, sfh, Zh, stellar_mass=stellar_mass)
 
     # --- create a galaxy object
     galaxy = Galaxy(sfzh)
@@ -43,17 +46,19 @@ def get_ew(index, Z, imf, grid, EqW, mode):
 
 def equivalent_width(grids, index, index_uv):
     # -- Calculate the equivalent width for each defined index
-    for i in range(0, len(index)):
+    for i, idx in enumerate(index):
         grid = Grid(grids, grid_dir=grid_dir)
 
         # --- define the parameters of the star formation and metal enrichment histories
-        Z = grid.metallicities
+        Z = grid.metallicity
         stellar_mass = 1E8
         EqW = []
 
         # Compute each index for each metallicity in the grid.
         for k in range(0, len(Z)):
-            EqW = get_ew(index[i], Z[k], stellar_mass, grid, EqW, 0)
+            EqW = get_ew(idx, Z[k], stellar_mass, grid, EqW, 0)
+
+        print(EqW)
 
         # Configure plot figure
         plt.rcParams['figure.dpi'] = 200
@@ -74,9 +79,9 @@ def equivalent_width(grids, index, index_uv):
 
         plt.title(label, fontsize=8, transform=plt.gca().transAxes, y=0.8)
 
-        plt.scatter(grid.metallicities, EqW, color='white', edgecolors='grey', alpha=1.0,
+        plt.scatter(grid.metallicity, EqW, color='white', edgecolors='grey', alpha=1.0,
                     zorder=10, linewidth=0.5, s=10)
-        plt.semilogx(grid.metallicities, EqW, linewidth=0.75, color='grey')
+        plt.semilogx(grid.metallicity, EqW, linewidth=0.75, color='grey')
         EqW.clear()
 
         plt.tight_layout()
@@ -84,26 +89,10 @@ def equivalent_width(grids, index, index_uv):
         if i == len(index) - 1:
             plt.show()
 
-
-def get_indices():
-    # -- Extract indices and pseudo continuum window from external csv file
-    index_grid = index()
-    temp = []
-    key = []
-
-    for i in range(0, len(index_grid)):
-        temp.append(index_grid[i][1:])
-        key.append(index_grid[i][0])
-
-    index_grid = temp
-
-    return index_grid, key
-
-
 if __name__ == '__main__':
-    grid_dir = '../tests/test_grid'  # Change this directory to your own.
+    grid_dir = '../../tests/test_grid'  # Change this directory to your own.
     grid_name = 'test_grid'  # Change this to the appropriate .hdf5
 
-    indices, uv_index = get_indices()  # Retrieve UV indices
+    indices = uv_indices()  # Retrieve UV indices
 
-    equivalent_width(grid_name, indices, uv_index)
+    equivalent_width(grid_name, indices, indices[:, 0])
