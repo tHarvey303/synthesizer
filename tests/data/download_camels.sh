3d925cf8261dd20423f1fc6a67409a94b7f04db9
christopherlovell <chrisl1991@hotmail.co.uk>
2023-09-11 09:05:59 -0400

    add smaller camels demo data (ignore first subhalo)

--- tests/data/download_camels.sh
+++ tests/data/download_camels.sh
@@ -3,7 +3,7 @@ dir=./
 # $1
 snap=031
 # $2
-lh=0
+lh=1
 # $3
 
 wget "https://users.flatironinstitute.org/~camels/Sims/IllustrisTNG/LH/LH_${lh}/snap_$snap.hdf5" --output-document "${dir}/LH_${lh}_snap_${snap}.hdf5"
