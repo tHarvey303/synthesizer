95b8b774d997061b57d404fb2c19a4fff871dc5c
wjr21 <wjr21@sussex.ac.uk>
2023-09-26 15:34:48 +0100

    Added Galaxy import for particle too

--- synthesizer/particle/__init__.py
+++ synthesizer/particle/__init__.py
@@ -2,3 +2,4 @@ from synthesizer.particle.stars import Stars
 from synthesizer.particle.gas import Gas
 from synthesizer.particle.blackholes.gas import BlackHoles
 from synthesizer.particle.particles import Particles as DarkMatter
+from synthesizer.particle.galaxy import Galaxy
