7e34d03344e136f6b112c90448f8fa81413bd5ee
wjr21 <wjr21@sussex.ac.uk>
2023-10-17 20:10:01 +0100

    Modified example to account for spectres

--- examples/grids/plot_interp_grid.py
+++ examples/grids/plot_interp_grid.py
@@ -29,30 +29,14 @@ if __name__ == "__main__":
 
     # Instantiate new grids
     start = time.time()
-    vhi_lin_grid = Grid(grid_name, grid_dir=grid_dir, new_lam=vhi_new_lams,
-                        interp_kind="linear")
-    print(f"Instatiating very high resolution (linear) grid took {time.time() - start:.2f} seconds")
+    vhi_lin_grid = Grid(grid_name, grid_dir=grid_dir, new_lam=vhi_new_lams)
+    print(f"Instatiating very high resolution grid took {time.time() - start:.2f} seconds")
     start = time.time()
-    hi_lin_grid = Grid(grid_name, grid_dir=grid_dir, new_lam=hi_new_lams,
-                       interp_kind="linear")
-    print(f"Instatiating high resolution (linear) grid took {time.time() - start:.2f} seconds")
+    hi_lin_grid = Grid(grid_name, grid_dir=grid_dir, new_lam=hi_new_lams)
+    print(f"Instatiating high resolution grid took {time.time() - start:.2f} seconds")
     start = time.time()
-    low_lin_grid = Grid(grid_name, grid_dir=grid_dir, new_lam=low_new_lams,
-                        interp_kind="linear")
-    print(f"Instatiating low resolution (linear) grid took {time.time() - start:.2f} seconds")
-
-    start = time.time()
-    vhi_cub_grid = Grid(grid_name, grid_dir=grid_dir, new_lam=vhi_new_lams,
-                        interp_kind="cubic")
-    print(f"Instatiating very high resolution (cubic) grid took {time.time() - start:.2f} seconds")
-    start = time.time()
-    hi_cub_grid = Grid(grid_name, grid_dir=grid_dir, new_lam=hi_new_lams,
-                       interp_kind="cubic")
-    print(f"Instatiating high resolution (cubic) grid took {time.time() - start:.2f} seconds")
-    start = time.time()
-    low_cub_grid = Grid(grid_name, grid_dir=grid_dir, new_lam=low_new_lams,
-                        interp_kind="cubic")
-    print(f"Instatiating low resolution (cubic) grid took {time.time() - start:.2f} seconds")
+    low_lin_grid = Grid(grid_name, grid_dir=grid_dir, new_lam=low_new_lams)
+    print(f"Instatiating low resolution grid took {time.time() - start:.2f} seconds")
 
     # Set up plot
     fig = plt.figure(figsize=(5, 3.5))
@@ -65,9 +49,6 @@ if __name__ == "__main__":
     ax.plot(vhi_lin_grid.lam, vhi_lin_grid.spectra["incident"][25, 5, :], label="Very High (Linear)")
     ax.plot(hi_lin_grid.lam, hi_lin_grid.spectra["incident"][25, 5, :], label="High (Linear)")
     ax.plot(low_lin_grid.lam, low_lin_grid.spectra["incident"][25, 5, :], label="Low (Linear)")
-    ax.plot(vhi_cub_grid.lam, vhi_cub_grid.spectra["incident"][25, 5, :], label="Very High (Cubic)")
-    ax.plot(hi_cub_grid.lam, hi_cub_grid.spectra["incident"][25, 5, :], label="High (Cubic)")
-    ax.plot(low_cub_grid.lam, low_cub_grid.spectra["incident"][25, 5, :], label="Low (Cubic)")
 
     ax.set_xlabel(r"$\lambda/\AA$")
     ax.set_ylabel(r"$L_{\nu}/erg\ s^{-1}\ Hz^{-1} M_{\odot}^{-1}$")
