05ecfa453467c103a379859d8ff720dcd5ef40ba
wjr21 <wjr21@sussex.ac.uk>
2023-11-09 18:13:31 +0000

    Initial clean up

--- src/synthesizer/parametric/stars.py
+++ src/synthesizer/parametric/stars.py
@@ -12,15 +12,51 @@ import matplotlib.pyplot as plt
 import cmasher as cmr
 
 from synthesizer import exceptions
+from synthesizer.components import StarsComponent
 from synthesizer.stats import weighted_median, weighted_mean
 from synthesizer.plt import single_histxy, mlabel
 
 
-class Stars:
+class Stars(StarsComponent):
+    """
+    The parametric stellar population object.
 
-    """this is a simple class for holding a binned star formation and metal enrichment history. This can be extended with other methods."""
+    This class holds a binned star formation and metal enrichment history
+    describing the age and metallicity of the stellar population, an
+    optional morphology model describing the distribution of those stars,
+    and various other important attributes for defining a parametric
+    stellar population.
 
-    def __init__(self, log10ages, metallicities, sfzh, sfh_f=None, Zh_f=None):
+    Attributes:
+        log10ages (array-like, float)
+        ages (Quantity, array-like, float)
+        log10ages_lims (array_like_float)
+        metallicities (array-like, float)
+        metallicities_lims (array-like, float)
+        log10metallicities (array-like, float)
+        log10metallicities_lims (array-like, float)
+        sfzh (array-like, float)
+        sf_hist (array-like, float)
+        metal_hist (array-like, float)
+        sf_hist_func (function)
+        metal_hist_func (function)
+    """
+
+    def __init__(
+            self,
+            log10ages,
+            metallicities,
+            sfzh,
+            morphology=None,
+            sf_hist_func=None,
+            metal_hist_func=None,
+    ):
+        """
+        Initialise the parametric stellar population.
+
+        Args:
+            log10ages
+        """
         self.log10ages = log10ages
         self.ages = 10**log10ages
         self.log10ages_lims = [self.log10ages[0], self.log10ages[-1]]
@@ -32,12 +68,12 @@ class Stars:
             self.log10metallicities[-1],
         ]
         self.sfzh = sfzh  # 2D star formation and metal enrichment history
-        self.sfh = np.sum(self.sfzh, axis=1)  # 1D star formation history
-        self.Z = np.sum(self.sfzh, axis=0)  # metallicity distribution
-        self.sfh_f = (
-            sfh_f  # function used to generate the star formation history if given
+        self.sf_hist = np.sum(self.sfzh, axis=1)  # 1D star formation history
+        self.metal_hist = np.sum(self.sfzh, axis=0)  # metallicity distribution
+        self.sf_hist_func = (
+            sf_hist_func  # function used to generate the star formation history if given
         )
-        self.Zh_f = Zh_f  # function used to generate the metallicity history/distribution if given
+        self.metal_hist_func = metal_hist_func  # function used to generate the metallicity history/distribution if given
 
         # --- check if metallicities on regular grid in log10metallicity or metallicity or not at all (e.g. BPASS
         if len(set(self.metallicities[:-1] - self.metallicities[1:])) == 1:
@@ -50,17 +86,17 @@ class Stars:
     def calculate_median_age(self):
         """calculate the median age"""
 
-        return weighted_median(self.ages, self.sfh) * yr
+        return weighted_median(self.ages, self.sf_hist) * yr
 
     def calculate_mean_age(self):
         """calculate the mean age"""
 
-        return weighted_mean(self.ages, self.sfh) * yr
+        return weighted_mean(self.ages, self.sf_hist) * yr
 
     def calculate_mean_metallicity(self):
         """calculate the mean metallicity"""
 
-        return weighted_mean(self.metallicities, self.Z)
+        return weighted_mean(self.metallicities, self.metal_hist)
 
     def __str__(self):
         """print basic summary of the binned star formation and metal enrichment history"""
@@ -80,10 +116,10 @@ class Stars:
         if second_sfzh.sfzh.shape == self.sfzh.shape:
             new_sfzh = self.sfzh + second_sfzh.sfzh
 
-            return BinnedSFZH(self.log10ages, self.metallicities, new_sfzh)
+            return Stars(self.log10ages, self.metallicities, new_sfzh)
 
         else:
-            exceptions.InconsistentAddition("SFZH must be the same shape")
+            raise exceptions.InconsistentAddition("SFZH must be the same shape")
 
     def plot(self, show=True):
         """Make a nice plots of the binned SZFH"""
@@ -98,25 +134,25 @@ class Stars:
         # --- add binned Z to right of the plot
         haxy.fill_betweenx(
             self.log10metallicities,
-            self.Z / np.max(self.Z),
+            self.metal_hist / np.max(self.metal_hist),
             step="mid",
             color="k",
             alpha=0.3,
         )
 
-        # --- add binned SFH to top of the plot
+        # --- add binned SF_HIST to top of the plot
         haxx.fill_between(
             self.log10ages,
-            self.sfh / np.max(self.sfh),
+            self.sf_hist / np.max(self.sf_hist),
             step="mid",
             color="k",
             alpha=0.3,
         )
 
         # --- add SFR to top of the plot
-        if self.sfh_f:
+        if self.sf_hist_func:
             x = np.linspace(*self.log10ages_lims, 1000)
-            y = self.sfh_f.sfr(10**x)
+            y = self.sf_hist_func.sfr(10**x)
             haxx.plot(x, y / np.max(y))
 
         haxy.set_xlim([0.0, 1.2])
@@ -137,23 +173,23 @@ class Stars:
         return fig, ax
 
 
-def generate_sfh(ages, sfh_, log10=False):
+def generate_sf_hist(ages, sf_hist_, log10=False):
     if log10:
         ages = 10**ages
 
-    SFH = np.zeros(len(ages))
+    SF_HIST = np.zeros(len(ages))
 
     min_age = 0
     for ia, age in enumerate(ages[:-1]):
         max_age = int(np.mean([ages[ia + 1], ages[ia]]))  #  years
-        sf = integrate.quad(sfh_.sfr, min_age, max_age)[0]
-        SFH[ia] = sf
+        sf = integrate.quad(sf_hist_.sfr, min_age, max_age)[0]
+        SF_HIST[ia] = sf
         min_age = max_age
 
     # --- normalise
-    SFH /= np.sum(SFH)
+    SF_HIST /= np.sum(SF_HIST)
 
-    return SFH
+    return SF_HIST
 
 
 def generate_instant_sfzh(
@@ -166,49 +202,49 @@ def generate_instant_sfzh(
     iZ = (np.abs(metallicities - metallicity)).argmin()
     sfzh[ia, iZ] = stellar_mass
 
-    return BinnedSFZH(log10ages, metallicities, sfzh)
+    return Stars(log10ages, metallicities, sfzh)
 
 
-def generate_sfzh(log10ages, metallicities, sfh, Zh, stellar_mass=1.0):
-    """return an instance of the BinnedSFZH class"""
+def generate_sfzh(log10ages, metallicities, sf_hist, metal_hist, stellar_mass=1.0):
+    """return an instance of the Stars class"""
 
     ages = 10**log10ages
 
     sfzh = np.zeros((len(log10ages), len(metallicities)))
 
-    if Zh.dist == "delta":
+    if metal_hist.dist == "delta":
         min_age = 0
         for ia, age in enumerate(ages[:-1]):
             max_age = int(np.mean([ages[ia + 1], ages[ia]]))  #  years
-            sf = integrate.quad(sfh.sfr, min_age, max_age)[0]
-            iZ = (np.abs(metallicities - Zh.Z(age))).argmin()
+            sf = integrate.quad(sf_hist.sfr, min_age, max_age)[0]
+            iZ = (np.abs(metallicities - metal_hist.Z(age))).argmin()
             sfzh[ia, iZ] = sf
             min_age = max_age
 
-    if Zh.dist == "dist":
+    if metal_hist.dist == "dist":
         print("WARNING: NOT YET IMPLEMENTED")
 
     # --- normalise
     sfzh /= np.sum(sfzh)
     sfzh *= stellar_mass
 
-    return BinnedSFZH(log10ages, metallicities, sfzh, sfh_f=sfh, Zh_f=Zh)
+    return Stars(log10ages, metallicities, sfzh, sf_hist_func=sf_hist, metal_hist_func=metal_hist)
 
 
-def generate_sfzh_from_array(log10ages, metallicities, sfh, Zh, stellar_mass=1.0):
+def generate_sfzh_from_array(log10ages, metallicities, sf_hist, metal_hist, stellar_mass=1.0):
     """
-    Generated a BinnedSFZH from an array instead of function
+    Generated a Stars from an array instead of function
     """
 
-    if not isinstance(Zh, np.ndarray):
-        iZ = np.abs(metallicities - Zh).argmin()
-        Zh = np.zeros(len(metallicities))
-        Zh[iZ] = 1.0
+    if not isinstance(metal_hist, np.ndarray):
+        iZ = np.abs(metallicities - metal_hist).argmin()
+        metal_hist = np.zeros(len(metallicities))
+        metal_hist[iZ] = 1.0
 
-    sfzh = sfh[:, np.newaxis] * Zh
+    sfzh = sf_hist[:, np.newaxis] * metal_hist
 
     # --- normalise
     sfzh /= np.sum(sfzh)
     sfzh *= stellar_mass
 
-    return BinnedSFZH(log10ages, metallicities, sfzh)
+    return Stars(log10ages, metallicities, sfzh)
