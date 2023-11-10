ee33e203dd40e53aaff784f98b0415344039402f
wjr21 <wjr21@sussex.ac.uk>
2023-11-10 16:45:32 +0000

    Updating easy imports

--- src/synthesizer/parametric/__init__.py
+++ src/synthesizer/parametric/__init__.py
@@ -1,6 +1,4 @@
-from synthesizer.parametric.stars import (
-    Stars, generate_instant_sfzh, generate_sfzh, generate_sfzh_from_array,
-)
+from synthesizer.parametric.stars import Stars
 from synthesizer.parametric.morphology import Sersic2D
 from synthesizer.parametric.galaxy import Galaxy
 from synthesizer.parametric.sf_hist import SFH, generate_sf_hist
