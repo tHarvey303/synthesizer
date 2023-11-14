f8eff733344d0f95ec6df4239e509710ba841768
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-11-14 14:01:34 +0000

    fixed bugs

--- src/synthesizer/blackholes.py
+++ src/synthesizer/blackholes.py
@@ -21,11 +21,11 @@ class UnifiedAGN:
                  spin=None,
                  metallicity=0.01,
                  U_blr=0.1,
-                 nH_blr=1E9/cm**3,
+                 n_H_blr=1E9/cm**3,
                  cov_blr=0.1,
                  v_blr=2000*km/s,
                  U_nlr=0.01,
-                 nH_nlr=1E4/cm**3,
+                 n_H_nlr=1E4/cm**3,
                  cov_nlr=0.1,
                  v_nlr=500*km/s,
                  theta_torus=10*deg,
@@ -57,7 +57,7 @@ class UnifiedAGN:
             U_blr (float)
                 The  ionisation parameter in the BLR. Default value
                 is 0.1.
-            nH_blr (unyt.unit_object.Unit)
+            n_H_blr (unyt.unit_object.Unit)
                 The  hydrogen density in the BLR. Default value
                 is 1E9/cm**3.
             cov_blr (float)
@@ -68,7 +68,7 @@ class UnifiedAGN:
             U_nlr (float)
                 The ionisation parameter in the BLR. Default value
                 is 0.01.
-            nH_nlr (unyt.unit_object.Unit)
+            n_H_nlr (unyt.unit_object.Unit)
                 The hydrogen density in the BLR. Default value
                 is 1E4/cm**3.
             cov_nlr (float)
@@ -90,17 +90,18 @@ class UnifiedAGN:
         self.log10MdotEdd = log10MdotEdd
         self.spin = spin,
         self.inclination = inclination
+        self.cosinc = inclination
         self.metallicity = metallicity
         self.U_blr = U_blr
         self.log10U_blr = np.log10(U_blr)
-        self.nH_blr = nH_blr
-        self.log10nH_blr = np.log10(nH_blr.to('1/cm**3').value)
+        self.n_H_blr = n_H_blr
+        self.log10n_H_blr = np.log10(n_H_blr.to('1/cm**3').value)
         self.cov_blr = cov_blr
         self.v_blr = v_blr
         self.U_nlr = U_nlr
         self.log10U_nlr = np.log10(U_nlr)
-        self.nH_nlr = nH_nlr
-        self.log10nH_nlr = np.log10(nH_nlr.to('1/cm**3').value)
+        self.n_H_nlr = n_H_nlr
+        self.log10n_H_nlr = np.log10(n_H_nlr.to('1/cm**3').value)
         self.cov_nlr = cov_nlr
         self.v_nlr = v_nlr
         self.theta_torus = theta_torus
@@ -123,7 +124,7 @@ class UnifiedAGN:
         for parameter in grid.axes:
 
             # for log10nH and log10U the parameters are labelled e.g. 'log10nH_nlr'
-            if parameter in ['log10nH', 'log10U']:
+            if parameter in ['log10n_H', 'log10U']:
                 parameter = parameter+'_'+gridt
             parameters.append(getattr(self, parameter))
 
@@ -145,17 +146,23 @@ class UnifiedAGN:
         # read the AGN grid
         # it will be better to encapsulate the 3 HDF5 files into a 
         # single HDF5 with different datasets for the disc, NLR, and BLR
-        nlr_grid = Grid(grid_name=f'{self.disc_model}-isotropic_cloudy-c17.03-nlr',
+        nlr_grid = Grid(grid_name=f'{self.disc_model}_cloudy-c17.03-nlr',
                          grid_dir=self.grid_dir,
                          read_lines=False)
         
-        blr_grid = Grid(grid_name=f'{self.disc_model}-isotropic_cloudy-c17.03-blr',
+        blr_grid = Grid(grid_name=f'{self.disc_model}_cloudy-c17.03-blr',
                     grid_dir=self.grid_dir,
                     read_lines=False)
         
         # get grid points
         nlr_grid_point = self._get_grid_point(nlr_grid, 'nlr')
         blr_grid_point = self._get_grid_point(blr_grid, 'blr')
+
+        # calculate the incident spectra. It doesn't matter which spectra we
+        # use here since we're just using the incident. Note: this assumes the 
+        # NLR and BLR are not overlapping.
+        self.spectra['disc_incident'] = \
+            nlr_grid.get_spectra(nlr_grid_point, spectra_id='incident')
 
         # calculate the transmitted spectra
         nlr_spectra = nlr_grid.get_spectra(nlr_grid_point, 
@@ -165,11 +172,9 @@ class UnifiedAGN:
         self.spectra['disc_transmitted'] = self.cov_nlr * nlr_spectra \
             + self.cov_blr * blr_spectra
             
-        # calculate the escaping spectra. It doesn't matter which spectra we
-        # use here since we're just using the incident. Note: this assumes the 
-        # NLR and BLR are not overlapping.
-        self.spectra['disc_escaped'] = (1 - self.cov_blr - self.cov_nlr) * \
-            nlr_grid.get_spectra(nlr_spectra, spectra_id='incident')
+        # calculate the escaping spectra. 
+        self.spectra['disc_escaped'] = (1 - self.cov_blr - self.cov_nlr) *\
+            self.spectra['disc_incident']
 
         # calculate the total spectra, the sum of escaping and transmitted
         self.spectra['disc'] = self.spectra['disc_transmitted'] + self.spectra['disc_escaped']
