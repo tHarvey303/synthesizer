b19f582f79aeb5cdb894a28d43a8a93e1ca630f9
wjr21 <wjr21@sussex.ac.uk>
2023-11-24 12:47:58 +0000

    Using spectres for sed resampling

--- src/synthesizer/utils.py
+++ src/synthesizer/utils.py
@@ -9,6 +9,9 @@ import numpy as np
 from unyt import c, h, kb, unyt_array, unyt_quantity
 
 
+from synthesizer import exceptions
+
+
 def planck(nu, temperature):
     """
     Planck's law.
@@ -49,3 +52,51 @@ def has_units(x):
         return True
 
     return False
+
+
+def rebin_1d(arr, resample_factor, func=np.sum):
+    """
+    A simple function for rebinning a 1D array using a specificed
+    function (e.g. sum or mean).
+
+    Args:
+        arr (array-like)
+            The input 1D array.
+        resample_factor (int)
+            The integer rebinning factor, i.e. how many bins to rebin by.
+        func (func)
+            The function to use (e.g. mean or sum).
+
+    Returns:
+        array-like
+            The input array resampled by i.
+    """
+
+    # Ensure the array is 1D
+    if arr.ndim != 1:
+        raise exceptions.InconsistentArguments(
+            f"Input array must be 1D (input was {arr.ndim}D)"
+        )
+
+    # Safely handle no integer resamples
+    if not isinstance(resample_factor, int):
+        print(
+            f"resample factor ({resample_factor}) is not an"
+            " integer, converting it to ",
+            end="\r",
+        )
+        resample_factor = int(resample_factor)
+        print(resample_factor)
+
+    # How many elements in the input?
+    n = len(arr)
+
+    # If array is not the right size truncate it
+    if n % resample_factor != 0:
+        arr = arr[: int(resample_factor * np.floor(n / resample_factor))]
+
+    # Set up the 2D array ready to have func applied
+    rows = len(arr) // resample_factor
+    brr = arr.reshape(rows, resample_factor)
+
+    return func(brr, axis=1)
