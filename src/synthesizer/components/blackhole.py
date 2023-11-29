0847026295a34012257cb3bcce97a8b382d35244
wjr21 <wjr21@sussex.ac.uk>
2023-11-29 14:50:05 +0000

    Removing slots

--- src/synthesizer/components/blackhole.py
+++ src/synthesizer/components/blackhole.py
@@ -22,14 +22,6 @@ class BlackholesComponent:
     This should never be instantiated directly.
 
     """
-    # TODO: reinstate this.
-    # Define the allowed attributes
-    # __slots__ = [
-    #     "_mass",
-    #     "_accretion_rate",
-    #     "_bb_temperature",
-    #     "_bolometric_luminosity",
-    # ]
 
     # Define class level Quantity attributes
     accretion_rate = Quantity()
@@ -51,8 +43,8 @@ class BlackholesComponent:
         metallicity=None,
     ):
         """
-        Initialise the BlackholeComponent. Where they're not provided missing 
-        quantities are automatically calcualted. Only some quantities are 
+        Initialise the BlackholeComponent. Where they're not provided missing
+        quantities are automatically calcualted. Only some quantities are
         needed for each emission model.
 
         Args:
@@ -74,8 +66,8 @@ class BlackholesComponent:
             metallicity (array-like, float)
                 The metallicity of the blackhole which is assumed for the line
                 emitting regions.
-            
-             
+
+
         """
 
         # Initialise spectra
@@ -91,30 +83,41 @@ class BlackholesComponent:
         self.bolometric_luminosity = bolometric_luminosity
         self.metallicity = metallicity
 
-        # Check to make sure that both accretion rate and bolometric luminosity
+        # Check to make sure that both accretion rate and bolometric luminosity
         # haven't been provided because that could be confusing.
-        if ((self.accretion_rate is not None)
-                and (self.bolometric_luminosity is not None)):
+        if (self.accretion_rate is not None) and (
+            self.bolometric_luminosity is not None
+        ):
             raise exceptions.InconsistentArguments(
                 """Both accretion rate and bolometric luminosity provided but
-                that is confusing. Provide one or the other!""")
+                that is confusing. Provide one or the other!"""
+            )
 
-        if ((self.accretion_rate_eddington is not None)
-                and (self.bolometric_luminosity is not None)):
+        if (self.accretion_rate_eddington is not None) and (
+            self.bolometric_luminosity is not None
+        ):
             raise exceptions.InconsistentArguments(
                 """Both accretion rate (in terms of Eddington) and bolometric
                 luminosity provided but that is confusing. Provide one or
-                the other!""")
+                the other!"""
+            )
 
         # If mass, accretion_rate, and epsilon provided calculate the
         # bolometric luminosity.
-        if ((self.mass is not None) and (self.accretion_rate is not None)
-                and (self.epsilon is not None)):
+        if (
+            (self.mass is not None)
+            and (self.accretion_rate is not None)
+            and (self.epsilon is not None)
+        ):
             self.calculate_bolometric_luminosity()
 
         # If mass, accretion_rate, and epsilon provided calculate the
         # big bump temperature.
-        if ((self.mass is not None) and (self.accretion_rate is not None) and (self.epsilon is not None)):
+        if (
+            (self.mass is not None)
+            and (self.accretion_rate is not None)
+            and (self.epsilon is not None)
+        ):
             self.calculate_bb_temperature()
 
         # If mass calculate the Eddington luminosity.
@@ -123,21 +126,27 @@ class BlackholesComponent:
 
         # If mass, accretion_rate, and epsilon provided calculate the
         # Eddington ratio.
-        if ((self.mass is not None) and (self.accretion_rate is not None) and (self.epsilon is not None)):
+        if (
+            (self.mass is not None)
+            and (self.accretion_rate is not None)
+            and (self.epsilon is not None)
+        ):
             self.calculate_eddington_ratio()
 
         # If mass, accretion_rate, and epsilon provided calculate the
         # accretion rate in units of the Eddington accretion rate. This is the
         # bolometric_luminosity / eddington_luminosity.
-        if ((self.mass is not None) and (self.accretion_rate is not None) and (self.epsilon is not None)):
+        if (
+            (self.mass is not None)
+            and (self.accretion_rate is not None)
+            and (self.epsilon is not None)
+        ):
             self.calculate_accretion_rate_eddington()
 
-        # If inclination, calculate the cosine of the inclination, required by 
+        # If inclination, calculate the cosine of the inclination, required by
         # some models (e.g. AGNSED).
-        if (self.inclination is not None):
-            self.cosine_inclination = np.cos(
-                self.inclination.to('radian').value)
-            
+        if self.inclination is not None:
+            self.cosine_inclination = np.cos(self.inclination.to("radian").value)
 
     def __str__(self):
         """Function to print a basic summary of the BlackHoles object.
@@ -160,22 +169,23 @@ class BlackholesComponent:
 
         pstr += f"Number of blackholes: {self.mass.size} \n"
 
-        for attribute_id in ['mass',
-                             'accretion_rate',
-                             'accretion_rate_eddington',
-                             'bolometric_luminosity',
-                             'eddington_ratio',
-                             'bb_temperature',
-                             'eddington_luminosity',
-                             'spin',
-                             'epsilon',
-                             'inclination',
-                             'cosine_inclination',
-                             ]:
+        for attribute_id in [
+            "mass",
+            "accretion_rate",
+            "accretion_rate_eddington",
+            "bolometric_luminosity",
+            "eddington_ratio",
+            "bb_temperature",
+            "eddington_luminosity",
+            "spin",
+            "epsilon",
+            "inclination",
+            "cosine_inclination",
+        ]:
             attr = getattr(self, attribute_id, None)
             if attr is not None:
                 attr = np.round(attr, 3)
-                pstr += f'{attribute_id}: {attr} \n'
+                pstr += f"{attribute_id}: {attr} \n"
 
         return pstr
 
@@ -183,7 +193,7 @@ class BlackholesComponent:
         """
         Calculate the black hole bolometric luminosity. This is by itself
         useful but also used for some emission models.
-        
+
         Returns
             unyt_array
                 The black hole bolometric luminosity
@@ -196,7 +206,7 @@ class BlackholesComponent:
     def calculate_eddington_luminosity(self):
         """
         Calculate the eddington luminosity of the black hole.
-        
+
         Returns
             unyt_array
                 The black hole bolometric luminosity
@@ -204,29 +214,28 @@ class BlackholesComponent:
 
         # Note: the factor 1.257E38 comes from:
         # 4*pi*G*mp*c*Msun/sigma_thompson
-        self.eddington_luminosity = 1.257E38 * self._mass 
-    
+        self.eddington_luminosity = 1.257e38 * self._mass
+
         return self.eddington_luminosity
-    
+
     def calculate_eddington_ratio(self):
         """
         Calculate the eddington ratio of the black hole.
-        
+
         Returns
             unyt_array
                 The black hole eddington ratio
         """
 
-        self.eddington_ratio = (self.bolometric_luminosity
-                                / self.eddington_luminosity)
-    
+        self.eddington_ratio = self.bolometric_luminosity / self.eddington_luminosity
+
         return self.eddington_ratio
-    
+
     def calculate_bb_temperature(self):
         """
         Calculate the black hole big bump temperature. This is used for the
         cloudy disc model.
-        
+
         Returns
             unyt_array
                 The black hole bolometric luminosity
@@ -242,25 +251,26 @@ class BlackholesComponent:
     def calculate_accretion_rate_eddington(self):
         """
         Calculate the black hole accretion in units of the Eddington rate.
-        
+
         Returns
             unyt_array
                 The black hole accretion rate in units of the Eddington rate.
         """
 
-        self.accretion_rate_eddington = (self._bolometric_luminosity /
-                                         self._eddington_luminosity)
+        self.accretion_rate_eddington = (
+            self._bolometric_luminosity / self._eddington_luminosity
+        )
 
         return self.accretion_rate_eddington
 
     def get_spectra(self, emission_model, spectra_ids=None, verbose=True):
         """
         Generate blackhole spectra for a given emission_model.
-        
+
         Args
             synthesizer.blackholes.BlackHoleEmissionModel
                 A synthesizer BlackHoleEmissionModel instance.
-        
+
         """
 
         # Get the parameters that this particular emission model requires
@@ -272,31 +282,32 @@ class BlackholesComponent:
                 emission_model_parameters[parameter] = attr
             elif priv_attr is not None:
                 emission_model_parameters[parameter] = priv_attr
-    
+
         if self.mass.size > 1:
             # Loop over the blackholes associated to the model
-            for i, values in enumerate(zip(*[x for x in emission_model_parameters.values()])):
-
+            for i, values in enumerate(
+                zip(*[x for x in emission_model_parameters.values()])
+            ):
                 # Create a dictionary of the parameters to be passed to the
-                # emission model.
+                # emission model.
                 parameters = dict(zip(emission_model_parameters.keys(), values))
 
-                # Get the parameters and spectra 
+                # Get the parameters and spectra
                 parameter_dict, spectra = emission_model.get_spectra(
                     spectra_ids=spectra_ids, **parameters
-                    )
-                
+                )
+
                 if self.spectra is None:
                     # Necessary so not a pointer
                     self.spectra = deepcopy(spectra)
-                else:       
+                else:
                     for spectra_id, spectra_ in spectra.items():
                         self.spectra[spectra_id] = self.spectra[spectra_id].concat(
-                            spectra_)
+                            spectra_
+                        )
         else:
             parameter_dict, self.spectra = emission_model.get_spectra(
-                    spectra_ids=spectra_ids, **emission_model_parameters
-                   )
+                spectra_ids=spectra_ids, **emission_model_parameters
+            )
 
         return self.spectra
-        
