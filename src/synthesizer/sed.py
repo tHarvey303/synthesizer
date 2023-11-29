41cfffec4373112f001e0676b8a565d285fec989
christopherlovell <chrisl1991@hotmail.co.uk>
2023-11-29 13:29:23 +0000

    undo initial fix

--- src/synthesizer/sed.py
+++ src/synthesizer/sed.py
@@ -855,8 +855,8 @@ class Sed:
         Measure an absorption feature index.
 
         Args:
-            absorption (tuple)
-                Absoprtion feature window.
+            feature (tuple)
+                Absorption feature window.
             blue (tuple)
                 Blue continuum window for fitting.
             red (tuple)
@@ -885,19 +885,18 @@ class Sed:
 
             # Set up output array
             index = np.zeros(len(self.lnu)) * self.lam.units
-                
-            continuum_fit = np.polyfit(
-                [np.mean(blue), np.mean(red)], [lnu_blue[i], lnu_red[i]], 1
-            )
-
-            # Use the continuum fit to define the continuum
-            continuum = (
-                (continuum_fit[0] * feature_lam.to(self.lam.units).value)
-                + continuum_fit[1]
-            ) * self.lnu.units
 
             # Note: I'm sure this could be done better.
             for i, _lnu in enumerate(self.lnu):
+                continuum_fit = np.polyfit(
+                    [np.mean(blue), np.mean(red)], [lnu_blue[i], lnu_red[i]], 1
+                )
+
+                # Use the continuum fit to define the continuum
+                continuum = (
+                    (continuum_fit[0] * feature_lam.to(self.lam.units).value)
+                    + continuum_fit[1]
+                ) * self.lnu.units
 
                 # Define the continuum subtracted spectrum
                 feature_lum = _lnu[transmission]
