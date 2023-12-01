1cdf4f97cbe017cb4308d1d1f1782211ed0c8403
wjr21 <wjr21@sussex.ac.uk>
2023-12-01 00:28:43 +0000

    Removing old fesc multiplication

--- src/synthesizer/extensions/integrated_spectra.c
+++ src/synthesizer/extensions/integrated_spectra.c
@@ -158,7 +158,8 @@ PyObject *compute_integrated_sed(PyObject *self, PyObject *args) {
     for (int ilam = 0; ilam < nlam; ilam++) {
 
       /* Add the contribution to this wavelength. */
-      spectra[ilam] += grid_spectra[spectra_ind + ilam] * (1 - fesc) * weight;
+      /* fesc is already included in the weight. */
+      spectra[ilam] += grid_spectra[spectra_ind + ilam] * weight;
     }
   }
 
