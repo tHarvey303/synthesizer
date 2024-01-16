536e3dc6e26eaac352e5ea2fcb710d4ae63ae484
Aswin P Vijayan <aswinpvijayan@github.com>
2024-01-17 00:23:01 +0100

    corrected the gdrive link

--- tests/get_test_grid.sh
+++ tests/get_test_grid.sh
@@ -9,4 +9,3 @@ gdown https://drive.google.com/uc?id=1XBUG04VUcuzRtQBR40J20RzDmFLu8EeX --output 
 gdown https://drive.google.com/uc?id=1k332yxFwyydlFn4XK-3J3lUTdimboYoN --output test_grid/test_grid_agn-blr.hdf5
 gdown https://drive.google.com/uc?id=1XQtJ4lEQUN-8Q_rfJCvcVBB4_3l1tCjU --output test_grid/test_grid_agn-nlr.hdf5
 gdown https://drive.google.com/uc?id=1FOs5S7rNPJP6NCe_p_BzFQ4Cckx4IKXL --output test_grid/MW3.1.hdf5
-https://drive.google.com/file/d/1FOs5S7rNPJP6NCe_p_BzFQ4Cckx4IKXL/view?usp=sharing
