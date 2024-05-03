3720b7313405ff3a1fb97cde776db18b1bc5a1eb
wjr21 <wjr21@sussex.ac.uk>
2024-05-03 15:10:05 +0100

    Making line ratios play nice

--- src/synthesizer/line_ratios.py
+++ src/synthesizer/line_ratios.py
@@ -13,16 +13,16 @@ standard, i.e. using vacuum wavelengths at <200nm and air wavelengths at
 Ha = "H 1 6562.80A"
 Hb = "H 1 4861.32A"
 Hg = "H 1 4340.46A"
-O1 = ["O 1 6300.30A"]
+O1 = "O 1 6300.30A"
 O2b = "O 2 3726.03A"
 O2r = "O 2 3728.81A"
-O2 = [O2b, O2r]
+O2 = O2b + "," + O2r
 O3b = "O 3 4958.91A"
 O3r = "O 3 5006.84A"
-O3 = [O3b, O3r]
-Ne3 = ["Ne 3 3868.76A"]
-N2 = ["N 2 6583.45A"]
-S2 = ["S 2 6730.82A", "S 2 6716.44A"]
+O3 = O3b + "," + O3r
+Ne3 = "Ne 3 3868.76A"
+N2 = "N 2 6583.45A"
+S2 = "S 2 6730.82A, S 2 6716.44A"
 
 
 # dictionary of common line labels to use by default
@@ -45,11 +45,11 @@ ratios["BalmerDecrement"] = [[Ha], [Hb]]
 # add reference
 ratios["N2"] = [N2, [Ha]]
 # add reference
-ratios["S2"] = [S2, [Ha]]
-ratios["O1"] = [O1, [Ha]]
+ratios["S2"] = [[S2], [Ha]]
+ratios["O1"] = [[O1], [Ha]]
 ratios["R2"] = [[O2b], [Hb]]
 ratios["R3"] = [[O3r], [Hb]]
-ratios["R23"] = [O3 + O2, [Hb]]
+ratios["R23"] = [[O3] + [O2], [Hb]]
 ratios["O32"] = [[O3r], [O2b]]
 ratios["Ne3O2"] = [Ne3, [O2b]]
 
