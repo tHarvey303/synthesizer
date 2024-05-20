06f2803a9c3bb0867a7a60c4ba78f3e4598f12a6
wjr21 <wjr21@sussex.ac.uk>
2024-05-20 17:36:42 +0100

    Included missing init

---
+++ src/synthesizer/emission_models/__init__.py
@@ -0,0 +1,14 @@
+from synthesizer.emission_models.base_model import EmissionModel
+from synthesizer.emission_models.models import (
+    IncidentEmission,
+    LineContinuumEmission,
+    TransmittedEmission,
+    EscapedEmission,
+    NebularContinuumEmission,
+    NebularEmission,
+    ReprocessedEmission,
+    AttenuatedEmission,
+    EmergentEmission,
+    DustEmission,
+    TotalEmission,
+)
