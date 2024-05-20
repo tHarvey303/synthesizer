6cebe65ed2e078c3443993daa235070702a380e2
wjr21 <wjr21@sussex.ac.uk>
2024-05-20 17:30:24 +0100

    Fully documented basic models

--- src/synthesizer/emission_models/models.py
+++ src/synthesizer/emission_models/models.py
@@ -23,12 +23,35 @@ from synthesizer.emission_models.base_model import EmissionModel
 
 
 class IncidentEmission(EmissionModel):
+    """
+    An emission model that extracts the incident radiation field.
+
+    This defines an extraction of key "incident" from a SPS grid.
+
+    This is a child of the EmissionModel class for a full description of the
+    parameters see the EmissionModel class.
+
+    Attributes:
+        grid (synthesizer.grid.Grid): The grid object to extract from.
+        label (str): The label for this emission model.
+        extract (str): The key to extract from the grid.
+        fesc (float): The escape fraction of the emission.
+    """
+
     def __init__(
         self,
         grid,
         label="incident",
         fesc=0.0,
     ):
+        """
+        Initialise the IncidentEmission object.
+
+        Args:
+            grid (synthesizer.grid.Grid): The grid object to extract from.
+            label (str): The label for this emission model.
+            fesc (float): The escape fraction of the emission.
+        """
         EmissionModel.__init__(
             self,
             grid=grid,
@@ -39,12 +62,35 @@ class IncidentEmission(EmissionModel):
 
 
 class LineContinuumEmission(EmissionModel):
+    """
+    An emission model that extracts the line continuum emission.
+
+    This defines an extraction of key "linecont" from a SPS grid.
+
+    This is a child of the EmissionModel class for a full description of the
+    parameters see the EmissionModel class.
+
+    Attributes:
+        grid (synthesizer.grid.Grid): The grid object to extract from.
+        label (str): The label for this emission model.
+        extract (str): The key to extract from the grid.
+        fesc (float): The escape fraction of the emission.
+    """
+
     def __init__(
         self,
         grid,
         label="linecont",
         fesc=0.0,
     ):
+        """
+        Initialise the LineContinuumEmission object.
+
+        Args:
+            grid (synthesizer.grid.Grid): The grid object to extract from.
+            label (str): The label for this emission model.
+            fesc (float): The escape fraction of the emission.
+        """
         EmissionModel.__init__(
             self,
             grid=grid,
@@ -55,12 +101,35 @@ class LineContinuumEmission(EmissionModel):
 
 
 class TransmittedEmission(EmissionModel):
+    """
+    An emission model that extracts the transmitted radiation field.
+
+    This defines an extraction of key "transmitted" from a SPS grid.
+
+    This is a child of the EmissionModel class for a full description of the
+    parameters see the EmissionModel class.
+
+    Attributes:
+        grid (synthesizer.grid.Grid): The grid object to extract from.
+        label (str): The label for this emission model.
+        extract (str): The key to extract from the grid.
+        fesc (float): The escape fraction of the emission.
+    """
+
     def __init__(
         self,
         grid,
         label="transmitted",
         fesc=0.0,
     ):
+        """
+        Initialise the TransmittedEmission object.
+
+        Args:
+            grid (synthesizer.grid.Grid): The grid object to extract from.
+            label (str): The label for this emission model.
+            fesc (float): The escape fraction of the emission.
+        """
         EmissionModel.__init__(
             self,
             grid=grid,
@@ -71,12 +140,41 @@ class TransmittedEmission(EmissionModel):
 
 
 class EscapedEmission(EmissionModel):
+    """
+    An emission model that extracts the escaped radiation field.
+
+    This defines an extraction of key "escaped" from a SPS grid.
+
+    This is a child of the EmissionModel class for a full description of the
+    parameters see the EmissionModel class.
+
+    Note: The escaped model is the "mirror" to the transmitted model. What
+    is transmitted is not escaped and vice versa. Therefore
+    EscapedEmission.fesc = 1 - TransmittedEmission.fesc. This will be
+    checked to be true at the time of spectra generation.
+
+    Attributes:
+        grid (synthesizer.grid.Grid): The grid object to extract from.
+        label (str): The label for this emission model.
+        extract (str): The key to extract from the grid.
+        fesc (float): The escape fraction of the emission.
+    """
+
     def __init__(
         self,
         grid,
         label="escaped",
         fesc=0.0,
     ):
+        """
+        Initialise the EscapedEmission object.
+
+        Args:
+            grid (synthesizer.grid.Grid): The grid object to extract from.
+            label (str): The label for this emission model.
+            fesc (float): The escape fraction of the emission. (Note that,
+                          1-fesc will be used during generation).
+        """
         EmissionModel.__init__(
             self,
             grid=grid,
@@ -87,12 +185,35 @@ class EscapedEmission(EmissionModel):
 
 
 class NebularContinuumEmission(EmissionModel):
+    """
+    An emission model that extracts the nebular continuum emission.
+
+    This defines an extraction of key "nebular_continuum" from a SPS grid.
+
+    This is a child of the EmissionModel class for a full description of the
+    parameters see the EmissionModel class.
+
+    Attributes:
+        grid (synthesizer.grid.Grid): The grid object to extract from.
+        label (str): The label for this emission model.
+        extract (str): The key to extract from the grid.
+        fesc (float): The escape fraction of the emission.
+    """
+
     def __init__(
         self,
         grid,
         label="nebular_continuum",
         fesc=0.0,
     ):
+        """
+        Initialise the NebularContinuumEmission object.
+
+        Args:
+            grid (synthesizer.grid.Grid): The grid object to extract from.
+            label (str): The label for this emission model.
+            fesc (float): The escape fraction of the emission.
+        """
         EmissionModel.__init__(
             self,
             grid=grid,
@@ -103,12 +224,36 @@ class NebularContinuumEmission(EmissionModel):
 
 
 class NebularEmission(EmissionModel):
+    """
+    An emission model that combines the nebular emission.
+
+    This defines a combination of the nebular continuum and line emission
+    components.
+
+    This is a child of the EmissionModel class for a full description of the
+    parameters see the EmissionModel class.
+
+    Attributes:
+        grid (synthesizer.grid.Grid): The grid object to extract from.
+        label (str): The label for this emission model.
+        combine (list): The emission models to combine.
+        fesc (float): The escape fraction of the emission.
+    """
+
     def __init__(
         self,
         grid,
         label="nebular",
         fesc=0.0,
     ):
+        """
+        Initialise the NebularEmission object.
+
+        Args:
+            grid (synthesizer.grid.Grid): The grid object to extract from.
+            label (str): The label for this emission model.
+            fesc (float): The escape fraction of the emission.
+        """
         EmissionModel.__init__(
             self,
             grid=grid,
@@ -122,12 +267,35 @@ class NebularEmission(EmissionModel):
 
 
 class ReprocessedEmission(EmissionModel):
+    """
+    An emission model that combines the reprocessed emission.
+
+    This defines a combination of the nebular and transmitted components.
+
+    This is a child of the EmissionModel class for a full description of the
+    parameters see the EmissionModel class.
+
+    Attributes:
+        grid (synthesizer.grid.Grid): The grid object to extract from.
+        label (str): The label for this emission model.
+        combine (list): The emission models to combine.
+        fesc (float): The escape fraction of the emission.
+    """
+
     def __init__(
         self,
         grid,
         label="reprocessed",
         fesc=0.0,
     ):
+        """
+        Initialise the ReprocessedEmission object.
+
+        Args:
+            grid (synthesizer.grid.Grid): The grid object to extract from.
+            label (str): The label for this emission model.
+            fesc (float): The escape fraction of the emission.
+        """
         EmissionModel.__init__(
             self,
             grid=grid,
@@ -141,6 +309,22 @@ class ReprocessedEmission(EmissionModel):
 
 
 class AttenuatedEmission(EmissionModel):
+    """
+    An emission model that defines the attenuated emission.
+
+    This defines the attenuation of the reprocessed emission by dust.
+
+    This is a child of the EmissionModel class for a full description of the
+    parameters see the EmissionModel class.
+
+    Attributes:
+        grid (synthesizer.grid.Grid): The grid object to extract from.
+        dust_curve (synthesizer.dust.DustCurve): The dust curve to use.
+        apply_dust_to (EmissionModel): The emission model to apply the dust to.
+        tau_v (float): The optical depth of the dust.
+        label (str): The label for this emission model.
+    """
+
     def __init__(
         self,
         grid,
@@ -149,6 +333,16 @@ class AttenuatedEmission(EmissionModel):
         tau_v,
         label="attenuated",
     ):
+        """
+        Initialise the AttenuatedEmission object.
+
+        Args:
+            grid (synthesizer.grid.Grid): The grid object to extract from.
+            dust_curve (synthesizer.dust.DustCurve): The dust curve to use.
+            apply_dust_to (EmissionModel): The model to apply the dust to.
+            tau_v (float): The optical depth of the dust.
+            label (str): The label for this emission model.
+        """
         EmissionModel.__init__(
             self,
             grid=grid,
@@ -160,6 +354,24 @@ class AttenuatedEmission(EmissionModel):
 
 
 class EmergentEmission(EmissionModel):
+    """
+    An emission model that defines the emergent emission.
+
+    This defines combination of the attenuated and escaped emission components
+    to produce the emergent emission.
+
+    This is a child of the EmissionModel class for a full description of the
+    parameters see the EmissionModel class.
+
+    Attributes:
+        grid (synthesizer.grid.Grid): The grid object to extract from.
+        dust_curve (synthesizer.dust.DustCurve): The dust curve to use.
+        apply_dust_to (EmissionModel): The emission model to apply the dust to.
+        tau_v (float): The optical depth of the dust.
+        fesc (float): The escape fraction of the emission.
+        label (str): The label for this emission model.
+    """
+
     def __init__(
         self,
         grid,
@@ -169,6 +381,17 @@ class EmergentEmission(EmissionModel):
         fesc=0.0,
         label="emergent",
     ):
+        """
+        Initialise the EmergentEmission object.
+
+        Args:
+            grid (synthesizer.grid.Grid): The grid object to extract from.
+            dust_curve (synthesizer.dust.DustCurve): The dust curve to use.
+            apply_dust_to (EmissionModel): The model to apply the dust to.
+            tau_v (float): The optical depth of the dust.
+            fesc (float): The escape fraction of the emission.
+            label (str): The label for this emission model.
+        """
         EmissionModel.__init__(
             self,
             grid=grid,
@@ -187,11 +410,33 @@ class EmergentEmission(EmissionModel):
 
 
 class DustEmission(EmissionModel):
+    """
+    An emission model that defines the dust emission.
+
+    This defines the dust emission model to use.
+
+    This is a child of the EmissionModel class for a full description of the
+    parameters see the EmissionModel class.
+
+    Attributes:
+        dust_emission_model (synthesizer.dust.DustEmissionModel): The dust
+            emission model to use.
+        label (str): The label for this emission model.
+    """
+
     def __init__(
         self,
         dust_emission_model,
         label="dust_emission",
     ):
+        """
+        Initialise the DustEmission object.
+
+        Args:
+            dust_emission_model (synthesizer.dust.DustEmissionModel): The dust
+                emission model to use.
+            label (str): The label for this emission model.
+        """
         EmissionModel.__init__(
             self,
             grid=None,
@@ -201,6 +446,25 @@ class DustEmission(EmissionModel):
 
 
 class TotalEmission(EmissionModel):
+    """
+    An emission model that defines the total emission.
+
+    This defines the combination of the emergent and dust emission components
+    to produce the total emission.
+
+    This is a child of the EmissionModel class for a full description of the
+    parameters see the EmissionModel class.
+
+    Attributes:
+        grid (synthesizer.grid.Grid): The grid object to extract from.
+        dust_curve (synthesizer.dust.DustCurve): The dust curve to use.
+        tau_v (float): The optical depth of the dust.
+        dust_emission_model (synthesizer.dust.DustEmissionModel): The dust
+            emission model to use.
+        label (str): The label for this emission model.
+        fesc (float): The escape fraction of the emission.
+    """
+
     def __init__(
         self,
         grid,
@@ -210,6 +474,18 @@ class TotalEmission(EmissionModel):
         label="total",
         fesc=0.0,
     ):
+        """
+        Initialise the TotalEmission object.
+
+        Args:
+            grid (synthesizer.grid.Grid): The grid object to extract from.
+            dust_curve (synthesizer.dust.DustCurve): The dust curve to use.
+            tau_v (float): The optical depth of the dust.
+            dust_emission_model (synthesizer.dust.DustEmissionModel): The dust
+                emission model to use.
+            label (str): The label for this emission model.
+            fesc (float): The escape fraction of the emission.
+        """
         # If a dust emission model has been passed then we need combine
         if dust_emission_model is not None:
             EmissionModel.__init__(
