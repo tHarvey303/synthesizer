f18cdd2eada3011e10b4d984ab24fc4d53238bb4
Aswin P Vijayan <aswinpvijayan@github.com>
2024-05-21 18:51:48 +0100

    moved verbose to init, added more warnings

--- src/synthesizer/particle/galaxy.py
+++ src/synthesizer/particle/galaxy.py
@@ -56,6 +56,7 @@ class Galaxy(BaseGalaxy):
         black_holes=None,
         redshift=None,
         centre=None,
+        verbose=True
     ):
         """Initialise a particle based Galaxy with objects derived from
            Particles.
@@ -91,6 +92,7 @@ class Galaxy(BaseGalaxy):
 
         # Set the type of galaxy
         self.galaxy_type = "Particle"
+        self.verbose = verbose
 
         # Instantiate the parent (load stars and gas below)
         BaseGalaxy.__init__(
@@ -137,6 +139,20 @@ class Galaxy(BaseGalaxy):
                     np.sum(self.stars.ages * self.stars.current_masses)
                     / self.stellar_mass
                 )
+            else:
+                self.stellar_mass_weighted_age = None
+                if self.verbose:
+                    print(
+                        "Ages of stars not provided, "
+                        "setting stellar_mass_weighted_age to `None`"
+                    )
+        else:
+            self.stellar_mass_weighted_age = None
+            if self.verbose:
+                print(
+                    "Current mass of stars not provided, "
+                    "setting stellar_mass_weighted_age to `None`"
+                )
 
     def calculate_integrated_gas_properties(self):
         """
@@ -175,7 +191,6 @@ class Galaxy(BaseGalaxy):
         ages=None,
         metallicities=None,
         stars=None,
-        verbose=True,
         **kwargs,
     ):
         """
@@ -191,8 +206,6 @@ class Galaxy(BaseGalaxy):
                 Star particle metallicity (total metal fraction)
             stars (stars particle object)
                 A pre-existing stars particle object to use. Defaults to None.
-            verbose (bool)
-                If True, print warnings and information messages.
             **kwargs
                 Arbitrary keyword arguments.
 
@@ -209,7 +222,7 @@ class Galaxy(BaseGalaxy):
                 | (ages is None)
                 | (metallicities is None)
             ):
-                if verbose:
+                if self.verbose:
                     print(
                         "In `load_stars`: one of either `initial_masses`"
                         ", `ages` or `metallicities` is not provided, setting "
@@ -234,7 +247,6 @@ class Galaxy(BaseGalaxy):
         masses=None,
         metallicities=None,
         gas=None,
-        verbose=True,
         **kwargs,
     ):
         """
@@ -248,8 +260,6 @@ class Galaxy(BaseGalaxy):
                 gas particle metallicity (total metal fraction)
             gas (gas particle object)
                 A pre-existing gas particle object to use. Defaults to None.
-            verbose (bool)
-                If True, print warnings and information messages.
         **kwargs
 
         Returns:
@@ -261,7 +271,7 @@ class Galaxy(BaseGalaxy):
         else:
             # If nothing has been provided, just set to None and return
             if (masses is None) | (metallicities is None):
-                if verbose:
+                if self.verbose:
                     print(
                         "In `load_stars`: one of either `masses`"
                         " or `metallicities` is not provided, setting "
