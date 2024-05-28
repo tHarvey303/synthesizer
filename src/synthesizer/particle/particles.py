f9ce4c3504f484b74ca05e725657ec4687c5bc85
wjr21 <wjr21@sussex.ac.uk>
2024-05-28 17:11:22 +0100

    Typo

--- src/synthesizer/particle/particles.py
+++ src/synthesizer/particle/particles.py
@@ -227,7 +227,7 @@ class Particles:
 
         return self.particle_photo_fluxes
 
-    def integate_particle_spectra(self):
+    def intergate_particle_spectra(self):
         """
         Integrate any particle spectra to get integrated spectra.
 
