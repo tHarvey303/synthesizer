a956ab62d09249043724846aacecb3f881e18e93
wjr21 <wjr21@sussex.ac.uk>
2024-06-11 17:00:40 +0100

    Implemented openmp for integrated lines

--- src/synthesizer/extensions/integrated_line.c
+++ src/synthesizer/extensions/integrated_line.c
@@ -44,71 +44,14 @@ static void store_weight(double weight, struct callback_data *data, void *out) {
 }
 
 /**
- * @brief Computes an integrated line emission for a collection of particles.
+ * @brief Compute the integrated line emission from the grid weights.
  *
- * @param np_grid_line: The SPS line emission array.
- * @param np_grid_continuum: The SPS continuum emission array.
- * @param grid_tuple: The tuple containing arrays of grid axis properties.
- * @param part_tuple: The tuple of particle property arrays (in the same order
- *                    as grid_tuple).
- * @param np_part_mass: The particle mass array.
- * @param fesc: The escape fraction.
- * @param np_ndims: The size of each grid axis.
- * @param ndim: The number of grid axes.
- * @param npart: The number of particles.
- * @param method: The method to use for assigning weights.
+ * @param grid_props: The grid properties.
+ * @param grid_weights: The grid weights computed from the particles.
+ *
+ * @return The integrated line emission.
  */
-PyObject *compute_integrated_line(PyObject *self, PyObject *args) {
-
-  /* We don't need the self argument but it has to be there. Tell the compiler
-   * we don't care. */
-  (void)self;
-
-  int ndim, npart;
-  PyObject *grid_tuple, *part_tuple;
-  PyArrayObject *np_grid_lines, *np_grid_continuum;
-  PyArrayObject *np_fesc;
-  PyArrayObject *np_part_mass, *np_ndims;
-  char *method;
-
-  if (!PyArg_ParseTuple(args, "OOOOOOOiis", &np_grid_lines, &np_grid_continuum,
-                        &grid_tuple, &part_tuple, &np_part_mass, &np_fesc,
-                        &np_ndims, &ndim, &npart, &method))
-    return NULL;
-
-  /* Extract the grid struct. */
-  struct grid *grid_props = get_lines_grid_struct(
-      grid_tuple, np_ndims, np_grid_lines, np_grid_continuum, ndim, /*nlam*/ 1);
-  if (grid_props == NULL) {
-    return NULL;
-  }
-
-  /* Extract the particle struct. */
-  struct particles *part_props =
-      get_part_struct(part_tuple, np_part_mass, np_fesc, npart, ndim);
-  if (part_props == NULL) {
-    return NULL;
-  }
-
-  /* Allocate an array to hold the grid weights. */
-  double *grid_weights = malloc(grid_props->size * sizeof(double));
-  if (grid_weights == NULL) {
-    PyErr_SetString(PyExc_MemoryError,
-                    "Failed to allocate memory for grid_weights.");
-    return NULL;
-  }
-  bzero(grid_weights, grid_props->size * sizeof(double));
-
-  /* With everything set up we can compute the weights for each particle using
-   * the requested method. */
-  if (strcmp(method, "cic") == 0) {
-    weight_loop_cic(grid_props, part_props, grid_weights, store_weight);
-  } else if (strcmp(method, "ngp") == 0) {
-    weight_loop_ngp(grid_props, part_props, grid_weights, store_weight);
-  } else {
-    PyErr_SetString(PyExc_ValueError, "Unknown grid assignment method (%s).");
-    return NULL;
-  }
+static double *get_lines_serial(struct grid *grid_props, double *grid_weights) {
 
   /* Declare and initialise the vairbales we'll store our result in. */
   double line_lum = 0.0;
@@ -129,6 +72,146 @@ PyObject *compute_integrated_line(PyObject *self, PyObject *args) {
     line_lum += grid_props->lines[grid_ind] * weight;
     line_cont += grid_props->continuum[grid_ind] * weight;
   }
+
+  /* Create an array to return. */
+  double result[2] = {line_lum, line_cont};
+  return result;
+}
+
+/**
+ * @brief Compute the integrated line emission from the grid weights.
+ *
+ * @param grid_props: The grid properties.
+ * @param grid_weights: The grid weights computed from the particles.
+ * @param nthreads: The number of threads to use.
+ */
+static double *get_lines_omp(struct grid *grid_props, double *grid_weights,
+                             int nthreads) {
+
+#ifdef WITH_OPENMP
+  /* Declare and initialise the vairbales we'll store our result in. */
+  double line_lum = 0.0;
+  double line_cont = 0.0;
+
+#pragma omp parallel num_threads(nthreads)
+  {
+
+    /* Loop over grid cells populating the lines. */
+#pragma omp for reduction(+ : line_lum, line_cont)
+    for (int grid_ind = 0; grid_ind < grid_props->size; grid_ind++) {
+
+      /* Get the weight. */
+      const double weight = grid_weights[grid_ind];
+
+      /* Skip zero weight cells. */
+      if (weight <= 0)
+        continue;
+
+      /* Add this grid cell's contribution to the lines (fesc is included in
+       * the weight).*/
+      line_lum += grid_props->lines[grid_ind] * weight;
+      line_cont += grid_props->continuum[grid_ind] * weight;
+    }
+  }
+
+  /* Create an array to return. */
+  double result[2] = {line_lum, line_cont};
+  return result;
+#else
+
+  /* We shouldn't be able to get here. */
+  PyErr_SetString(PyExc_RuntimeError,
+                  "OpenMP not enabled but in an OpenMP function.");
+  return NULL;
+#endif
+}
+
+/**
+ * @brief Computes an integrated line emission for a collection of particles.
+ *
+ * @param np_grid_line: The SPS line emission array.
+ * @param np_grid_continuum: The SPS continuum emission array.
+ * @param grid_tuple: The tuple containing arrays of grid axis properties.
+ * @param part_tuple: The tuple of particle property arrays (in the same order
+ *                    as grid_tuple).
+ * @param np_part_mass: The particle mass array.
+ * @param fesc: The escape fraction.
+ * @param np_ndims: The size of each grid axis.
+ * @param ndim: The number of grid axes.
+ * @param npart: The number of particles.
+ * @param method: The method to use for assigning weights.
+ */
+PyObject *compute_integrated_line(PyObject *self, PyObject *args) {
+
+  /* We don't need the self argument but it has to be there. Tell the compiler
+   * we don't care. */
+  (void)self;
+
+  int ndim, npart, nthreads;
+  PyObject *grid_tuple, *part_tuple;
+  PyArrayObject *np_grid_lines, *np_grid_continuum;
+  PyArrayObject *np_fesc;
+  PyArrayObject *np_part_mass, *np_ndims;
+  char *method;
+
+  if (!PyArg_ParseTuple(args, "OOOOOOOiis", &np_grid_lines, &np_grid_continuum,
+                        &grid_tuple, &part_tuple, &np_part_mass, &np_fesc,
+                        &np_ndims, &ndim, &npart, &method, &nthreads))
+    return NULL;
+
+  /* Extract the grid struct. */
+  struct grid *grid_props = get_lines_grid_struct(
+      grid_tuple, np_ndims, np_grid_lines, np_grid_continuum, ndim, /*nlam*/ 1);
+  if (grid_props == NULL) {
+    return NULL;
+  }
+
+  /* Extract the particle struct. */
+  struct particles *part_props =
+      get_part_struct(part_tuple, np_part_mass, np_fesc, npart, ndim);
+  if (part_props == NULL) {
+    return NULL;
+  }
+
+  /* With everything set up we can compute the weights for each particle using
+   * the requested method. */
+  double *grid_weights;
+  if (strcmp(method, "cic") == 0) {
+    grid_weights = weight_loop_cic(grid_props, part_props, grid_props->size,
+                                   store_weight, nthreads);
+  } else if (strcmp(method, "ngp") == 0) {
+    grid_weights = weight_loop_ngp(grid_props, part_props, grid_props->size,
+                                   store_weight, nthreads);
+  } else {
+    PyErr_SetString(PyExc_ValueError, "Unknown grid assignment method (%s).");
+    return NULL;
+  }
+
+  /* Check we got grid weights sucessfully. (Any error messages will have
+   * already been set) */
+  if (grid_weights == NULL) {
+    return NULL;
+  }
+
+#ifdef WITH_OPENMP
+  /* Do we have mutliple threads to do the reduction onto the lines? */
+  if (nthreads > 1) {
+    double *result = get_lines_omp(grid_props, grid_weights, nthreads);
+    if (result == NULL) {
+      return NULL;
+    }
+  } else {
+    double *result = get_lines_serial(grid_props, grid_weights);
+  }
+#else
+  /* We don't have OpenMP so we can't do the reduction in parallel. */
+  double *result = get_lines_serial(grid_props, grid_weights);
+#endif
+
+  /* Unpack the result array (it's 2 elements containing the line and
+   * continuum). */
+  double line_lum = result[0];
+  double line_cont = result[1];
 
   /* Clean up memory! */
   free(grid_weights);
