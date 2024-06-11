5d3f1e37c75c47947faa0f1891c2efddc7302957
wjr21 <wjr21@sussex.ac.uk>
2024-06-11 09:52:10 +0100

    Brought over simple profile scripts

---
+++ profiling/profile_mem_int_spectra.py
@@ -0,0 +1,90 @@
+"""A script to profile the memory usage of a integrated spectra calculation.
+
+Usage:
+    python profile_mem_int_spectra.py 1000 --basename test
+"""
+
+import argparse
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+from synthesizer.grid import Grid
+from synthesizer.parametric import SFH, ZDist
+from synthesizer.parametric import Stars as ParametricStars
+from synthesizer.particle.stars import sample_sfhz
+from unyt import Myr
+
+plt.rcParams["font.family"] = "DeJavu Serif"
+plt.rcParams["font.serif"] = ["Times New Roman"]
+
+# Set the seed
+np.random.seed(42)
+
+
+def profile_mem_int_spectra(n, basename):
+    """Profile the memory usage of the integrated spectra calculation."""
+    start = time.time()
+
+    # Define the grid
+    grid_name = "test_grid"
+    grid_dir = "../tests/test_grid/"
+    grid = Grid(grid_name, grid_dir=grid_dir)
+
+    # Define the grid (normally this would be defined by an SPS grid)
+    log10ages = np.arange(6.0, 10.5, 0.1)
+    metallicities = 10 ** np.arange(-5.0, -1.5, 0.1)
+    metal_dist = ZDist.Normal(0.01, 0.005)
+    sfh = SFH.Constant(100 * Myr)  # constant star formation
+
+    # Generate the star formation metallicity history
+    mass = 10**10
+    param_stars = ParametricStars(
+        log10ages,
+        metallicities,
+        sf_hist=sfh,
+        metal_dist=metal_dist,
+        initial_mass=mass,
+    )
+
+    # Setup lists for times
+    times = []
+
+    # Sample the SFZH, producing a Stars object
+    stars = sample_sfhz(
+        param_stars.sfzh,
+        param_stars.log10ages,
+        param_stars.log10metallicities,
+        n,
+        current_masses=np.full(n, 10**8.7 / n),
+        redshift=1,
+    )
+
+    stars.get_spectra_incident(grid)
+
+    print(f"{n} stars took", time.time() - start)
+    times.append(time.time() - start)
+
+    np.savetxt(f"{basename}_{n}_integrated_mem_times.txt", times)
+
+
+if __name__ == "__main__":
+    # Get the command line args
+    args = argparse.ArgumentParser()
+
+    args.add_argument(
+        "n",
+        type=int,
+        help="The number of stars to generate.",
+    )
+
+    args.add_argument(
+        "--basename",
+        type=str,
+        default="test",
+        help="The basename of the output files.",
+    )
+
+    args = args.parse_args()
+
+    profile_mem_int_spectra(args.n, args.basename)
