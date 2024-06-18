5d16e5c7e07cd86c181706f7aa6719733436ceaf
wjr21 <wjr21@sussex.ac.uk>
2024-06-18 18:50:38 +0100

    Updated inits

--- src/synthesizer/emission_models/__init__.py
+++ src/synthesizer/emission_models/__init__.py
@@ -1,4 +1,8 @@
 from synthesizer.emission_models.base_model import EmissionModel
+from synthesizer.emission_models.models import (
+    DustEmission,
+    AttenuatedEmission,
+)
 from synthesizer.emission_models.stellar.models import (
     IncidentEmission,
     LineContinuumEmission,
@@ -7,9 +11,7 @@ from synthesizer.emission_models.stellar.models import (
     NebularContinuumEmission,
     NebularEmission,
     ReprocessedEmission,
-    AttenuatedEmission,
     EmergentEmission,
-    DustEmission,
     TotalEmission,
 )
 from synthesizer.emission_models.stellar.pacman_model import (
@@ -17,20 +19,28 @@ from synthesizer.emission_models.stellar.pacman_model import (
     BimodalPacmanEmission,
     CharlotFall2000,
 )
+from synthesizer.emission_models.agn.models import (
+    Template,
+    NLRIncidentEmission,
+    BLRIncidentEmission,
+    NLRTransmittedEmission,
+    BLRTransmittedEmission,
+    DiscIncidentEmission,
+    DiscTransmittedEmission,
+    DiscEscapedEmission,
+    DiscEmission,
+    TorusEmission,
+    AGNIntrinsicEmission,
+    UnifiedAGN,
+)
+
+
+from synthesizer.emission_models.stellar import STELLAR_MODELS
+from synthesizer.emission_models.agn import AGN_MODELS
 
 PREMADE_MODELS = [
-    "IncidentEmission",
-    "LineContinuumEmission",
-    "TransmittedEmission",
-    "EscapedEmission",
-    "NebularContinuumEmission",
-    "NebularEmission",
-    "ReprocessedEmission",
     "AttenuatedEmission",
-    "EmergentEmission",
     "DustEmission",
-    "TotalEmission",
-    "PacmanEmission",
-    "BimodalPacmanEmission",
-    "CharlotFall2000",
 ]
+PREMADE_MODELS.extend(STELLAR_MODELS)
+PREMADE_MODELS.extend(AGN_MODELS)
---
+++ src/synthesizer/emission_models/agn/__init__.py
@@ -0,0 +1,14 @@
+AGN_MODELS = [
+    "Template",
+    "NLRIncidentEmission",
+    "BLRIncidentEmission",
+    "NLRTransmittedEmission",
+    "BLRTransmittedEmission",
+    "DiscIncidentEmission",
+    "DiscTransmittedEmission",
+    "DiscEscapedEmission",
+    "DiscEmission",
+    "TorusEmission",
+    "AGNIntrinsicEmission",
+    "UnifiedAGN",
+]
