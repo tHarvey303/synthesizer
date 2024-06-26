Fixed multidimensional spectra scaling

src/synthesizer/emission_models/operations.py
@@ -321,9 +321,17 @@ class Generation:
             )
 
             # scale the spectra by the intrinsic luminosity
-            spectra[this_model.label]._lnu *= spectra[
-                this_model.lum_intrinsic_model.label
-            ].bolometric_luminosity.value
+            if spectra[this_model.label].ndim == 2:
+                spectra[this_model.label]._lnu *= np.expand_dims(
+                    spectra[
+                        this_model.lum_intrinsic_model.label
+                    ].bolometric_luminosity.value,
+                    axis=1,
+                )
+            else:
+                spectra[this_model.label]._lnu *= spectra[
+                    this_model.lum_intrinsic_model.label
+                ].bolometric_luminosity.value
 
         else:
             # Otherwise we have a bog standard generation