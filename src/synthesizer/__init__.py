404504fa08a4fcd6ef2d30107a3271b7cc087ce7
wjr21 <wjr21@sussex.ac.uk>
2024-07-04 11:05:18 +0100

    Moving openmp checker to init

--- src/synthesizer/__init__.py
+++ src/synthesizer/__init__.py
@@ -3,3 +3,5 @@ from synthesizer import abundances
 from synthesizer.galaxy import galaxy
 from synthesizer.galaxy import galaxy as Galaxy
 from synthesizer.grid import Grid
+
+from synthesizer.extensions.openmp_check import check_openmp
