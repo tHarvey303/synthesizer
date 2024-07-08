e3b7e2d459701a54021870256c6d4e32e4d8dbd6
wjr21 <wjr21@sussex.ac.uk>
2024-07-08 15:56:38 +0100

    Fixed igm data paths

--- src/synthesizer/emission_models/attenuation/igm.py
+++ src/synthesizer/emission_models/attenuation/igm.py
@@ -32,8 +32,8 @@ class Inoue14:
         path = os.path.join(os.path.dirname(filepath), "data")
         # print path
 
-        LAF_file = os.path.join(path, "LAFcoeff.txt")
-        DLA_file = os.path.join(path, "DLAcoeff.txt")
+        LAF_file = os.path.join(path, "../../data/LAFcoeff.txt")
+        DLA_file = os.path.join(path, "../../data/DLAcoeff.txt")
 
         data = np.loadtxt(LAF_file, unpack=True)
         ix, lam, ALAF1, ALAF2, ALAF3 = data
