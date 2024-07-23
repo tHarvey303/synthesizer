85aec7a98b4e2fdfe7a3b4762d5f6e6ce2525f2e
christopherlovell <chrisl1991@hotmail.co.uk>
2024-07-23 13:43:48 +0100

    update exmaple

--- examples/parametric/plot_generate_lines.py
+++ examples/parametric/plot_generate_lines.py
@@ -7,72 +7,84 @@ parametric galaxy.
 """
 
 import synthesizer.line_ratios as line_ratios
+from synthesizer.emission_models import IncidentEmission, AttenuatedEmission
 from synthesizer.grid import Grid
 from synthesizer.parametric import SFH, Stars, ZDist
 from synthesizer.parametric.galaxy import Galaxy
 from unyt import Myr
 
-# Begin by defining and initialising the grid. By setting `read_spectra`
-# to `False` we can avoid reading in the spectra reducing the memory footprint.
-grid_name = "test_grid"
-grid_dir = "../../../tests/test_grid/"
-grid = Grid(grid_name, grid_dir=grid_dir, read_spectra=False)
+if __name__ == "__main__":
+    # Begin by defining and initialising the grid. By setting `read_spectra`
+    # to `False` we can avoid reading in the spectra reducing the memory footprint.
+    grid_name = "test_grid"
+    grid_dir = "../../tests/test_grid/"
+    grid = Grid(grid_name, grid_dir=grid_dir)  # , read_spectra=False)
 
-# Let's now build a galaxy following the other tutorials:
-# Define the functional form of the star formation and metal
-# enrichment histories
+    # Define the emission model
+    model = IncidentEmission(grid)
 
-# Constant star formation
-sfh = SFH.Constant(duration=100 * Myr)
+    # Let's now build a galaxy following the other tutorials:
+    # Define the functional form of the star formation and metal
+    # enrichment histories
 
-# Constant metallicity
-metal_dist = ZDist.DeltaConstant(log10metallicity=-2.0)
+    # Constant star formation
+    sfh = SFH.Constant(duration=100 * Myr)
 
-# Get the 2D star formation and metal enrichment history
-# for the given SPS grid. This is (age, Z).
-stars = Stars(
-    grid.log10age,
-    grid.metallicity,
-    sf_hist=sfh,
-    metal_dist=metal_dist,
-    initial_mass=10**8.5,
-)
+    # Constant metallicity
+    metal_dist = ZDist.DeltaConstant(log10metallicity=-2.0)
 
-# Create the Galaxy object
-galaxy = Galaxy(stars)
+    # Get the 2D star formation and metal enrichment history
+    # for the given SPS grid. This is (age, Z).
+    stars = Stars(
+        grid.log10age,
+        grid.metallicity,
+        sf_hist=sfh,
+        metal_dist=metal_dist,
+        initial_mass=10**8.5,
+    )
 
-# Print a summary of the Galaxy object
-print(galaxy)
+    # Create the Galaxy object
+    galaxy = Galaxy(stars)
 
-# Let's define a list of lines that we're interested in. Note that we can
-# provide multiples which are automatically summed as if they were blended.
-line_ids = [
-    line_ratios.Hb,  # "H 1 4861.32A"
-    line_ratios.O3b,  # "O 3 4958.91A"
-    line_ratios.O3r,  # "O 3 5006.84A"
-    line_ratios.O3,  # ["O 3 4958.91A", "O 3 5006.84A"]
-    line_ratios.O3
-    + ","
-    + line_ratios.Hb,  # ["O 3 4958.91A", "O 3 5006.84A", "H 1 4861.32A"]
-]
+    # Print a summary of the Galaxy object
+    print(galaxy)
 
-# Next, let's get the intrinsic line properties:
-lines = galaxy.stars.get_line_intrinsic(grid, line_ids)
+    # Let's define a list of lines that we're interested in. Note that we can
+    # provide multiples which are automatically summed as if they were blended.
+    line_ids = [
+        line_ratios.Hb,  # "H 1 4861.32A"
+        line_ratios.O3b,  # "O 3 4958.91A"
+        line_ratios.O3r,  # "O 3 5006.84A"
+        line_ratios.O3,  # ["O 3 4958.91A", "O 3 5006.84A"]
+        line_ratios.O3
+        + ","
+        + line_ratios.Hb,  # ["O 3 4958.91A", "O 3 5006.84A", "H 1 4861.32A"]
+    ]
 
-# This produces a LineCollection object which has some useful methods and
-# information.
-print(lines)
+    # Next, let's get the intrinsic line properties:
+    lines = galaxy.stars.get_lines(line_ids, model)
 
-# Let's now examine individual lines (or doublets):
-for line in lines:
-    print(line)
+    # This produces a LineCollection object which has some useful methods and
+    # information.
+    print(lines)
 
-# Those lines are now associated with the `Galaxy` object, revealed by
-# using the print function:
-print(galaxy)
+    # Let's now examine individual lines (or doublets):
+    for line in lines:
+        print(line)
 
-# Next, lets get the attenuated line properties:
-lines_att = galaxy.stars.get_line_attenuated(
-    grid, line_ids, fesc=0.0, tau_v_nebular=0.0, tau_v_stellar=0.5
-)
-print(lines_att)
+    # Those lines are now associated with the `Galaxy` object, revealed by
+    # using the print function:
+    print(galaxy)
+
+    # Next, lets get the attenuated line properties:
+    model = AttenuatedEmission(
+        grid,
+        emitter='stellar',
+        tau_v=1.0,
+        apply_dust_to='intrinsic',
+    )
+
+    lines_att = galaxy.stars.get_lines(
+        line_ids, model,
+    )
+    print(lines_att)
