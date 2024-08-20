75bc4bfe0b4acbacb7dd855d6e123d3cd017be0c
wjr21 <wjr21@sussex.ac.uk>
2024-08-21 00:52:47 +0100

    Fixing linecollection creation

--- src/synthesizer/emission_models/base_model.py
+++ src/synthesizer/emission_models/base_model.py
@@ -2574,7 +2574,7 @@ class EmissionModel(Extraction, Generation, DustAttenuation, Combination):
                 # conversion
                 if isinstance(lines[label], dict):
                     lines[label] = LineCollection(lines[label])
-                if this_model.per_particle and isinstance(
+                if self.models[label].per_particle and isinstance(
                     particle_lines[label], dict
                 ):
                     particle_lines[label] = LineCollection(
