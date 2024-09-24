Fixing units in examples

examples/general/plot_filters.py
@@ -6,11 +6,12 @@ A demonstration of filter and filter collections creation and usage.
 """
 
 import numpy as np
+from unyt import angstrom
 
 from synthesizer.filters import UVJ, Filter, FilterCollection
 
 # Define some wavelengths (in A) over which we want to define our filters
-lams = np.linspace(2000, 55000, 1000)
+lams = np.linspace(2000, 55000, 1000) * angstrom
 
 # Define an individual filter from SVO.
 filt = Filter("JWST/NIRCam.F200W", new_lam=lams)

examples/particle/plot_rotated_galaxy.py
@@ -16,7 +16,7 @@ galaxy.
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import cKDTree
-from unyt import Myr, degree, kpc
+from unyt import Msun, Myr, degree, km, kpc, s
 
 from synthesizer.kernel_functions import Kernel
 from synthesizer.particle import CoordinateGenerator, Galaxy, Gas, Stars
@@ -73,12 +73,12 @@ velocities = np.vstack([disk_velocities, bulge_velocities])
 
 
 # Define the other properties we'll need
-masses = np.ones(n_disk + n_bulge) * 1e6
+masses = np.ones(n_disk + n_bulge) * 1e6 * Msun
 ages = np.random.rand(n_disk + n_bulge) * 100 * Myr
 metallicities = np.random.rand(n_disk + n_bulge) * 0.02
 initial_masses = masses.copy()
 redshift = 0.0
-centre = np.array([0.0, 0.0, 0.0])
+centre = np.array([0.0, 0.0, 0.0]) * kpc
 smoothing_lengths = calculate_smoothing_lengths(coords) * kpc
 
 # We'll start by simply using some stars
@@ -140,8 +140,8 @@ gas_cov = np.array(
     ]
 )
 gas_coords = CoordinateGenerator.generate_3D_gaussian(ngas, cov=gas_cov)
-gas_velocities = np.random.normal(0, 50, size=(ngas, 3))
-gas_masses = np.ones(ngas) * 1e6
+gas_velocities = np.random.normal(0, 50, size=(ngas, 3)) * km / s
+gas_masses = np.ones(ngas) * 1e6 * Msun
 gas_metallcities = np.random.rand(ngas) * 0.02
 gas = Gas(
     gas_masses,