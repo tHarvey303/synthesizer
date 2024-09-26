12ad65a43b715c6898ad41b0d25b253ef564d782
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-09-26 14:40:56 +0100

    added scaler check

--- src/synthesizer/emission_models/base_model.py
+++ src/synthesizer/emission_models/base_model.py
@@ -2622,6 +2622,7 @@ class EmissionModel(Extraction, Generation, DustAttenuation, Combination):
                 if scaler is None:
                     continue
                 elif hasattr(emitter, scaler):
+                    print(scaler)
                     for line_id in line_ids:
                         if this_model.per_particle:
                             particle_lines[label][
