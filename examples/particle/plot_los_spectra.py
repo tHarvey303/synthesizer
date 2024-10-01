Fixed all units

examples/particle/plot_los_spectra.py
@@ -11,7 +11,7 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import cKDTree
-from unyt import Myr
+from unyt import Msun, Myr
 
 from synthesizer.emission_models import TotalEmission
 from synthesizer.emission_models.attenuation import PowerLaw
@@ -75,7 +75,7 @@ sfh_p = {"duration": 100 * Myr}
 sfh = SFH.Constant(**sfh_p)  # constant star formation
 
 # Generate the star formation metallicity history
-mass = 10**10
+mass = 10**10 * Msun
 param_stars = ParametricStars(
     log10ages,
     metallicities,