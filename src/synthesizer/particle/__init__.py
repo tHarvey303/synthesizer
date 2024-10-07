4e34ef87b76635cb7abdff70cd2fc9b23689c6e4
wjr21 <wjr21@sussex.ac.uk>
2024-10-08 00:07:20 +0100

    Avoiding unused error in init

--- src/synthesizer/particle/__init__.py
+++ src/synthesizer/particle/__init__.py
@@ -3,3 +3,12 @@ from synthesizer.particle.galaxy import Galaxy
 from synthesizer.particle.particles import CoordinateGenerator, Particles
 from synthesizer.particle.particles import Particles as DarkMatter
 from synthesizer.particle.stars import Stars
+
+__all__ = [
+    BlackHoles,
+    Galaxy,
+    CoordinateGenerator,
+    Particles,
+    DarkMatter,
+    Stars,
+]
