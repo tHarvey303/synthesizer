0ba757da3d1fde7e24f1b33b97aec55cb1fd237d
wjr21 <wjr21@sussex.ac.uk>
2024-10-09 10:37:20 +0100

    Clean up init

--- src/synthesizer/instruments/__init__.py
+++ src/synthesizer/instruments/__init__.py
@@ -1,13 +1,6 @@
 from synthesizer.instruments.filters import UVJ, Filter, FilterCollection
-
-# We have to import InstrumentCollection here before Instrument to avoid
-# circular imports
-from synthesizer.instruments.instrument_collection import InstrumentCollection
-
-# Now we can import Instrument
 from synthesizer.instruments.instrument import Instrument
-
-
+from synthesizer.instruments.instrument_collection import InstrumentCollection
 
 __all__ = [
     "Instrument",
