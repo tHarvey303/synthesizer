47d3ecc13da400d150e04c47788dd8d4169dad2b
wjr21 <wjr21@sussex.ac.uk>
2025-01-15 16:20:12 +0000

    Removing reference to old naming scheme

--- src/synthesizer/grid.py
+++ src/synthesizer/grid.py
@@ -1084,10 +1084,7 @@ class Grid:
         y = np.arange(len(self.metallicity))
 
         # Select grid for specific ion
-        if hasattr(self, "log10_specific_ionising_lum"):
-            log10_specific_ionising_lum = self.log10_specific_ionising_lum[ion]
-        else:
-            log10_specific_ionising_lum = self.log10Q[ion]
+        log10_specific_ionising_lum = self.log10_specific_ionising_lum[ion]
 
         # Truncate grid if max age provided
         if max_log10age is not None:
