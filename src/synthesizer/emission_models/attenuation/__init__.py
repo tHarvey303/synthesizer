418afc3e4d5cda9e0044440ae0cbaa00634890a4
wjr21 <wjr21@sussex.ac.uk>
2025-02-06 18:55:00 +0000

    Friendly alias submodule for backwards compatibility

--- src/synthesizer/emission_models/attenuation/__init__.py
+++ src/synthesizer/emission_models/attenuation/__init__.py
@@ -1,2 +1,14 @@
-# Unpack the attenuation laws into the parent package
-from synthesizer.emission_models.attenuation.dust import *
+# Unpack the attenuation laws into this nice alias submodule
+from synthesizer.emission_models.transformers.dust_attenuation import (
+    MWN18,
+    Calzetti2000,
+    GrainsWD01,
+    ParametricLi08,
+    PowerLaw,
+)
+
+# Unpack the IGM transformers into this nice alias submodule
+from synthesizer.emission_models.transformers.igm import (
+    Inoue14,
+    Madau96,
+)
