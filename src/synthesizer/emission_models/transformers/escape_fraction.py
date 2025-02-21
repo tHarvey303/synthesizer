5914ffa1881c0a58e80752f2a9586b08bb27e6f3
wjr21 <wjr21@sussex.ac.uk>
2025-02-21 11:15:01 +0000

    Adding flexibility to Escape fraction transformer

--- src/synthesizer/emission_models/transformers/escape_fraction.py
+++ src/synthesizer/emission_models/transformers/escape_fraction.py
@@ -20,11 +20,19 @@ class EscapeFraction(Transformer):
     must match the shape of 1D fesc arrays.
     """
 
-    def __init__(self):
-        """Initialise the escape fraction transformer."""
+    def __init__(self, fesc_attrs=("fesc",)):
+        """
+        Initialise the escape fraction transformer.
+
+        Args:
+            fesc_attrs (tuple, optional):
+                The attributes to extract from the model to use as the escape
+                fraction. If multiple are passed these will be added
+                toegether. Default is ("fesc",).
+        """
         # Call the parent class constructor and declare we need fesc for this
         # transformer.
-        Transformer.__init__(self, required_params=("fesc",))
+        Transformer.__init__(self, required_params=fesc_attrs)
 
     def _transform(self, emission, emitter, model, mask, lam_mask):
         """
@@ -59,8 +67,11 @@ class EscapeFraction(Transformer):
                 f"emission.shape: {emission.shape})."
             )
 
+        # Combine the escape fractions
+        fesc = sum([params[attr] for attr in self.required_params])
+
         return emission.scale(
-            1 - params["fesc"],
+            1 - fesc,
             mask=mask,
             lam_mask=lam_mask,
         )
@@ -82,8 +93,16 @@ class EscapedFraction(Transformer):
     must match the shape of 1D fesc arrays.
     """
 
-    def __init__(self):
-        """Initialise the escape fraction transformer."""
+    def __init__(self, fesc_attrs=("fesc",)):
+        """
+        Initialise the escaped fraction transformer.
+
+        Args:
+            fesc_attrs (tuple, optional):
+                The attributes to extract from the model to use as the escape
+                fraction. If multiple are passed these will be added
+                together. Default is ("fesc",).
+        """
         # Call the parent class constructor and declare we need fesc for this
         # transformer.
         Transformer.__init__(self, required_params=("fesc",))
@@ -121,8 +140,86 @@ class EscapedFraction(Transformer):
                 f"emission.shape: {emission.shape})."
             )
 
+        # Combine the escape fractions
+        fesc = sum([params[attr] for attr in self.required_params])
+
         return emission.scale(
-            params["fesc"],
+            fesc,
             mask=mask,
             lam_mask=lam_mask,
         )
+
+
+# Define aliases for the AGN covering fraction transformers. These are just
+# the same as the escape fraction transformers but with the default attributes
+# set to the covering fraction attributes.
+
+
+class CoveringFraction(EscapedFraction):
+    """
+    A transformer that applies a covering fraction to an emission.
+
+    This is an alias for the AGN covering fraction which is effectively the
+    EscapedFraction transformer (i.e. it transforms disc emission to get the
+    emisison covered and trasmitted through a line region).
+
+    This can be thought of as an attenuation law with a single value, reducing
+    the emission by a constant factor.
+
+    Note that the inverse (the escaped emission) can be achieved using the
+    EscapingFraction transformer. This is effectively identical to this
+    transformer but will apply fesc instead of (1 - fesc), as the scaling.
+
+    If fesc is an array then the shape of the emission must match along all
+    axes accept the final one for multi-dimensional emissions, 1D emissions
+    must match the shape of 1D fesc arrays.
+    """
+
+    def __init__(self, fesc_attrs):
+        """
+        Initialise the covering fraction transformer.
+
+        Args:
+            fesc_attrs (tuple, optional):
+                The attributes to extract from the model to use as the escape
+                fraction. If multiple are passed these will be added
+                together. We pass no default here since there is ambiguity.
+        """
+        # Call the parent class constructor and declare we need fesc for this
+        # transformer.
+        EscapeFraction.__init__(self, fesc_attrs=fesc_attrs)
+
+
+class EscapingFraction(EscapeFraction):
+    """
+    A transformer that applies a covering fraction to an emission.
+
+    This is an alias for the AGN covering fraction which is effectively the
+    EscapeFraction transformer (i.e. it transforms disc emission to get the
+    emisison not covered by a line region).
+
+    This can be thought of as an attenuation law with a single value, reducing
+    the emission by a constant factor.
+
+    Note that the inverse (the escaped emission) can be achieved using the
+    EscapingFraction transformer. This is effectively identical to this
+    transformer but will apply fesc instead of (1 - fesc), as the scaling.
+
+    If fesc is an array then the shape of the emission must match along all
+    axes accept the final one for multi-dimensional emissions, 1D emissions
+    must match the shape of 1D fesc arrays.
+    """
+
+    def __init__(self, fesc_attrs):
+        """
+        Initialise the covering fraction transformer.
+
+        Args:
+            fesc_attrs (tuple, optional):
+                The attributes to extract from the model to use as the escape
+                fraction. If multiple are passed these will be added
+                together. We pass no default here since there is ambiguity.
+        """
+        # Call the parent class constructor and declare we need fesc for this
+        # transformer.
+        EscapeFraction.__init__(self, fesc_attrs=fesc_attrs)
