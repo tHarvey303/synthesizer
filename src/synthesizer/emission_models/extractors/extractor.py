Fixed parametric line extraction (and lines units on grid)

src/synthesizer/emission_models/extractors/extractor.py
@@ -790,6 +790,7 @@ class IntegratedParametricExtractor(Extractor):
 
         # Compute the integrated line array by multiplying the sfzh by
         # the grids
+        print(type(grid_line_lums), lam_mask)
         lum = np.sum(grid_line_lums[mask] * sfzh[mask], axis=0)
         cont = np.sum(grid_line_conts[mask] * sfzh[mask], axis=0)
 
@@ -798,6 +799,6 @@ class IntegratedParametricExtractor(Extractor):
         return LineCollection(
             line_ids=self._grid.line_ids,
             lam=self._line_lams,
-            lum=lum * erg / s,
-            cont=cont * erg / s / Hz,
+            lum=lum,
+            cont=cont,
         )

src/synthesizer/grid.py
@@ -640,12 +640,16 @@ class Grid:
                 # the continuum at the line wavelengths (we use scipy here
                 # because spectres can't handle single value interpolation).
                 # The linecont continuum is explicitly 0 and set above.
-                self.line_conts[spectra] = interp_func(self.line_lams)
+                self.line_conts[spectra] = unyt_array(
+                    interp_func(self.line_lams),
+                    cont_units,
+                )
 
                 # Set the line luminosities to 0 as long as they haven't
                 # already been set
-                self.line_lums[spectra] = np.zeros(
-                    self.spectra[spectra].shape[:-1]
+                self.line_lums[spectra] = unyt_array(
+                    np.zeros(self.spectra[spectra].shape[:-1]),
+                    lum_units,
                 )
 
     def _prepare_lam_axis(