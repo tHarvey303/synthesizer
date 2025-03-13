487907a680f7ac87cbec3b49221464e6b9d78e53
wjr21 <wjr21@sussex.ac.uk>
2025-03-13 11:39:20 -0500

    Operation tests

---
+++ tests/test_operations.py
@@ -0,0 +1,55 @@
+"""A suite of tests for the emission model operations."""
+
+import numpy as np
+
+
+def test_single_star_extraction(
+    single_star_particle,
+    single_star_parametric,
+    test_grid,
+    reprocessed_emission_model,
+):
+    """
+    Test extraciton of a single star's emission.
+
+    This will use and compare a single star for a particle Stars object and a
+    single SFZH bin for a parametric Stars object. These two descriptions
+    should be equivalent.
+    """
+    # First ensure the sfzh's are equivalent
+    single_star_particle.get_sfzh(
+        test_grid.log10ages,
+        test_grid.log10metallicity,
+    )
+    assert np.isclose(np.sum(single_star_particle.sfzh.sfzh), 1.0), (
+        "The unit particle SFZH does not sum to 1"
+        f" (sum={np.sum(single_star_particle.sfzh.sfzh.sum())})"
+    )
+    assert np.isclose(np.sum(single_star_parametric.sfzh), 1.0), (
+        "The unit parametric SFZH does not sum to 1"
+        f" (sum={np.sum(single_star_parametric.sfzh)})"
+    )
+    assert np.allclose(
+        single_star_particle.sfzh.sfzh,
+        single_star_parametric.sfzh,
+    ), (
+        f"The SFZH's are not equivalent (non-zero elements: "
+        f"particle={np.where(single_star_particle.sfzh.sfzh > 0)}, "
+        f"parametric={np.where(single_star_parametric.sfzh >0)})"
+    )
+
+    # Ok, we know the SFZH's are equivalent, let's now get the spectra
+    # and compare them.
+    part_sed = single_star_particle.get_spectra(
+        reprocessed_emission_model,
+    )
+    param_sed = single_star_parametric.get_spectra(
+        reprocessed_emission_model,
+    )
+    assert np.allclose(
+        part_sed.lnu,
+        param_sed.lnu,
+    ), (
+        "The SEDs are not equivalent (part_sed - param_sed "
+        f"={np.sum(part_sed.lnu) - np.sum(param_sed.lnu)})"
+    )
