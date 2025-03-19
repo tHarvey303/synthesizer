fafbf9b89e56595443a3c8d49ce0543ba815b218
wjr21 <wjr21@sussex.ac.uk>
2025-03-19 11:49:56 +0000

    Providing higher level imports for emission utils

--- src/synthesizer/emissions/__init__.py
+++ src/synthesizer/emissions/__init__.py
@@ -12,3 +12,16 @@ for alias, line in line_aliases.items():
 # Import the emissions classe
 from synthesizer.emissions.line import LineCollection
 from synthesizer.emissions.sed import Sed
+
+# Import some important utility functions
+from synthesizer.emissions.utils import (
+    combine_list_of_seds,
+    get_attenuation,
+    get_attenuation_at_1500,
+    get_attenuation_at_5500,
+    get_attenuation_at_lam,
+    get_transmission,
+    plot_observed_spectra,
+    plot_spectra,
+    plot_spectra_as_rainbow,
+)
