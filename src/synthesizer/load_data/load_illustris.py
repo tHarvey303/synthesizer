4d74948085e3decb3266286c9b64e62712ae7e39
Will Roper <wjr21@sussex.ac.uk>
2025-03-19 22:34:04 +0000

    Using absolute import path

--- src/synthesizer/load_data/load_illustris.py
+++ src/synthesizer/load_data/load_illustris.py
@@ -28,7 +28,7 @@ from astropy.cosmology import Planck15
 from tqdm import tqdm
 from unyt import Msun, kpc, yr
 
-from .utils import age_lookup_table, lookup_age
+from synthesizer.load_data.utils import age_lookup_table, lookup_age
 from synthesizer.exceptions import UnmetDependency
 
 try:
