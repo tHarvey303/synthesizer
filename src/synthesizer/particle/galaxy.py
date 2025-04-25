fix units on zero valued derived properties

src/synthesizer/particle/galaxy.py
@@ -184,8 +184,8 @@ class Galaxy(BaseGalaxy):
         if self.gas.star_forming is not None:
             mask = self.gas.star_forming
             if np.sum(mask) == 0:
-                self.sf_gas_mass = 0.0
-                self.sf_gas_metallicity = 0.0
+                self.sf_gas_mass = 0.0 * Msun
+                self.sf_gas_metallicity = 0.0 * Msun
             else:
                 self.sf_gas_mass = np.sum(self.gas.masses[mask])
 