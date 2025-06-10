0c7ade3f4f76a63f1ddfa3550f1eb89b734af96b
christopherlovell <chrisl1991@hotmail.co.uk>
2025-06-10 17:45:23 +0100

    fix ruff issues

--- src/synthesizer/load_data/load_simba.py
+++ src/synthesizer/load_data/load_simba.py
@@ -21,95 +21,41 @@ def load_Simba(
     snap_name="snap_033.hdf5",
     caesar_name="fof_subhalo_tab_033.hdf5",
     caesar_directory=None,
+    object_indexes=False,
     load_halo=False,
     age_lookup=True,
     age_lookup_delta_a=1e-4,
 ):
     """Load Simba galaxy data from a caesar file and snapshot.
 
+    This function can load all objects or a specific subset based on their
+    indices. It uses the `slist` and `glist` datasets from the Caesar file
+    to correctly map particles to their host galaxies/halos.
+
     Args:
         directory (string):
-            data location
+            Data location.
         snap_name (string):
-            snapshot filename
+            Snapshot filename.
         caesar_name (string):
-            Subfind / FOF filename
+            Caesar file name.
         caesar_directory (string):
-            optional argument specifying location of fof file
-            if different to snapshot
-        load_halo (bool, false):
-            optional argument, whether to load galaxy or halo objects.
-            If False (default), will load individual galaxies.
+            Optional path to the Caesar file if it's in a different
+            directory from the snapshot.
+        object_indexes (list or bool):
+            List of object indices to load. If False, all objects are loaded.
+        load_halo (bool):
+            If True, loads halo objects instead of galaxy objects.
         age_lookup (bool):
-            Create a lookup table for ages
+            If True, creates and uses a lookup table for stellar ages for
+            faster age calculation.
         age_lookup_delta_a (float):
-            Scale factor resolution of the age lookup
+            The resolution of the age lookup table in terms of scale factor.
 
     Returns:
-        galaxies (object):
-            `ParticleGalaxy` object containing star and gas particles
+        galaxies (list):
+            A list of `synthesizer.particle.galaxy.Galaxy` objects.
     """
-    with h5py.File(f"{directory}/{snap_name}", "r") as hf:
-        scale_factor = hf["Header"].attrs["Time"]
-        Om0 = hf["Header"].attrs["Omega0"]
-        h = hf["Header"].attrs["HubbleParam"]
-
-        form_time = hf["PartType4/StellarFormationTime"][:]
-        coods = hf["PartType4/Coordinates"][:] * scale_factor  # Mpc (physical)
-        masses = hf["PartType4/Masses"][:]
-
-        imasses = np.ones(len(masses)) * 0.00155
-        # * hf['Header'].attrs['MassTable'][1]
-
-        _metals = hf["PartType4/Metallicity"][:]
-
-        # g_sfr = hf["PartType0/StarFormationRate"][:]
-        g_h2fraction = hf["PartType0/FractionH2"][:]
-        g_masses = hf["PartType0/Masses"][:]
-        g_metals = hf["PartType0/Metallicity"][:][:, 0]
-        g_coods = (
-            hf["PartType0/Coordinates"][:] * scale_factor
-        )  # Mpc (physical)
-        g_hsml = hf["PartType0/SmoothingLength"][:]  # Mpc
-
-        g_dustmass = hf["PartType0/Dust_Masses"][:]
-
-    redshift = (1.0 / scale_factor) - 1
-
-    # convert units
-    masses = (masses * 1e10) / h
-    g_masses = (g_masses * 1e10) / h
-    g_dustmass = (g_dustmass * 1e10) / h
-    imasses = (imasses * 1e10) / h
-
-    # get individual and summed metallicity components
-    s_oxygen = _metals[:, 4]
-    s_hydrogen = 1 - np.sum(_metals[:, 1:], axis=1)
-    metallicity = _metals[:, 0]
-
-    # convert formation times to ages
-    cosmo = FlatLambdaCDM(H0=h * 100, Om0=Om0)
-    universe_age = cosmo.age(1.0 / scale_factor - 1)
-
-    # Are we creating a lookup table for ages?
-    if age_lookup:
-        # Create the lookup grid
-        scale_factors, ages = age_lookup_table(
-            cosmo,
-            redshift=redshift,
-            delta_a=age_lookup_delta_a,
-            low_lim=1e-4,
-        )
-
-        # Look up the ages for the particles
-        _ages = lookup_age(form_time, scale_factors, ages)
-    else:
-        # Calculate ages of these explicitly using astropy
-        _ages = cosmo.age(1.0 / form_time - 1)
-
-    # Calculate ages at snapshot redshift
-    ages = (universe_age - _ages).value * 1e9  # yr
-
     # check which kind of object we're loading
     if load_halo:
         obj_str = "halo_data"
@@ -119,48 +65,146 @@ def load_Simba(
     if caesar_directory is None:
         caesar_directory = directory
 
-    # get the star particle begin / end indices
+    # Open the Caesar file to get particle lists for each galaxy
     with h5py.File(f"{caesar_directory}/{caesar_name}", "r") as hf:
-        begin = hf[f"{obj_str}/slist_start"][:]
-        end = hf[f"{obj_str}/slist_end"][:]
+        slist_all = hf[f"{obj_str}/lists/slist"][:]
+        slist_start_all = hf[f"{obj_str}/slist_start"][:]
+        slist_end_all = hf[f"{obj_str}/slist_end"][:]
 
-    galaxies = [None] * len(begin)
-    for i, (b, e) in enumerate(zip(begin, end)):
-        # create the individual galaxy objects
-        galaxies[i] = Galaxy()
-        galaxies[i].redshift = redshift
-        galaxies[i].load_stars(
-            initial_masses=imasses[b:e] * Msun,
-            ages=ages[b:e] * yr,
-            metallicities=metallicity[b:e],
-            s_oxygen=s_oxygen[b:e],
-            s_hydrogen=s_hydrogen[b:e],
-            coordinates=coods[b:e, :] * kpc,
-            current_masses=masses[b:e] * Msun,
+        glist_all = hf[f"{obj_str}/lists/glist"][:]
+        glist_start_all = hf[f"{obj_str}/glist_start"][:]
+        glist_end_all = hf[f"{obj_str}/glist_end"][:]
+
+        centres_all = hf[f"{obj_str}/pos"][:]
+
+    # Get the particle indices for the selected galaxies
+    if object_indexes is False:
+        galaxy_indices = np.arange(len(slist_start_all))
+
+        star_particle_indices = slist_all
+        gas_particle_indices = glist_all
+    else:
+        galaxy_indices = np.array(object_indexes)
+
+        star_particle_indices = np.concatenate(
+            [
+                slist_all[slist_start_all[i] : slist_end_all[i]]
+                for i in galaxy_indices
+            ]
+        )
+        gas_particle_indices = np.concatenate(
+            [
+                glist_all[glist_start_all[i] : glist_end_all[i]]
+                for i in galaxy_indices
+            ]
         )
 
-    # get the gas particle begin / end indices
-    with h5py.File(f"{caesar_directory}/{caesar_name}", "r") as hf:
-        begin = hf[f"{obj_str}/glist_start"][:]
-        end = hf[f"{obj_str}/glist_end"][:]
+    centres = centres_all[galaxy_indices]
 
-    for i, (b, e) in enumerate(zip(begin, end)):
-        # Use the H2 masses computed in Simba directly to
-        # estimate the star forming gas mass and metallicity.
-        # Alternative to setting star_forming property on
-        # each gas particle
-        h2_masses = g_masses[b:e] * g_h2fraction[b:e]
-        galaxies[i].sf_gas_mass = np.sum(h2_masses) * Msun
-        galaxies[i].sf_gas_metallicity = (
-            np.sum(h2_masses * g_metals[b:e]) / galaxies[i].sf_gas_mass
+    with h5py.File(f"{directory}/{snap_name}", "r") as hf:
+        scale_factor = hf["Header"].attrs["Time"]
+        Om0 = hf["Header"].attrs["Omega0"]
+        h = hf["Header"].attrs["HubbleParam"]
+
+        # Load star data for selected particles
+        form_time = hf["PartType4/StellarFormationTime"][:][
+            star_particle_indices
+        ]
+        coods = (
+            hf["PartType4/Coordinates"][:][star_particle_indices]
+            * scale_factor
+            / h
+        )
+        masses = hf["PartType4/Masses"][:][star_particle_indices]
+        imasses = np.ones(len(masses)) * 0.00155
+        _metals = hf["PartType4/Metallicity"][:][star_particle_indices]
+
+        # Load gas data for selected particles
+        g_masses = hf["PartType0/Masses"][:][gas_particle_indices]
+        g_metals_all = hf["PartType0/Metallicity"][:][gas_particle_indices]
+        g_metals = g_metals_all[:, 0]
+        g_coods = (
+            hf["PartType0/Coordinates"][:][gas_particle_indices]
+            * scale_factor
+            / h
+        )
+        g_hsml = hf["PartType0/SmoothingLength"][:][gas_particle_indices]
+        g_dustmass = hf["PartType0/Dust_Masses"][:][gas_particle_indices]
+        g_h2fraction = hf["PartType0/FractionH2"][:][gas_particle_indices]
+
+    redshift = (1.0 / scale_factor) - 1
+
+    # Convert units
+    masses = (masses * 1e10) / h
+    imasses = (imasses * 1e10) / h
+    g_masses = (g_masses * 1e10) / h
+    g_dustmass = (g_dustmass * 1e10) / h
+
+    # Get individual and summed metallicity components for stars
+    s_oxygen = _metals[:, 4]
+    s_hydrogen = 1 - np.sum(_metals[:, 1:], axis=1)
+    metallicity = _metals[:, 0]
+
+    # Convert formation times to ages
+    cosmo = FlatLambdaCDM(H0=h * 100, Om0=Om0)
+    universe_age = cosmo.age(redshift)
+
+    if age_lookup:
+        scale_factors, ages_table = age_lookup_table(
+            cosmo, redshift=redshift, delta_a=age_lookup_delta_a, low_lim=1e-4
+        )
+        _ages = lookup_age(form_time, scale_factors, ages_table)
+    else:
+        _ages = cosmo.age(1.0 / form_time - 1)
+
+    ages = (universe_age - _ages).value * 1e9  # yr
+
+    # Prepare to create Galaxy objects
+    galaxies = []
+    s_particle_counts = [
+        slist_end_all[i] - slist_start_all[i] for i in galaxy_indices
+    ]
+    g_particle_counts = [
+        glist_end_all[i] - glist_start_all[i] for i in galaxy_indices
+    ]
+    s_offsets = np.concatenate([[0], np.cumsum(s_particle_counts)[:-1]])
+    g_offsets = np.concatenate([[0], np.cumsum(g_particle_counts)[:-1]])
+
+    for i, gal_idx in enumerate(galaxy_indices):
+        galaxy = Galaxy()
+        galaxy.redshift = redshift
+
+        s_start, s_end = s_offsets[i], s_offsets[i] + s_particle_counts[i]
+        g_start, g_end = g_offsets[i], g_offsets[i] + g_particle_counts[i]
+
+        galaxy.load_stars(
+            initial_masses=imasses[s_start:s_end] * Msun,
+            ages=ages[s_start:s_end] * yr,
+            metallicities=metallicity[s_start:s_end],
+            s_oxygen=s_oxygen[s_start:s_end],
+            s_hydrogen=s_hydrogen[s_start:s_end],
+            coordinates=coods[s_start:s_end, :] * kpc,
+            current_masses=masses[s_start:s_end] * Msun,
+            centre=centres[i] * kpc,
         )
 
-        galaxies[i].load_gas(
-            coordinates=g_coods[b:e] * kpc,
-            masses=g_masses[b:e] * Msun,
-            metallicities=g_metals[b:e],
-            smoothing_lengths=g_hsml[b:e] * kpc,
-            dust_masses=g_dustmass[b:e] * Msun,
+        h2_masses = g_masses[g_start:g_end] * g_h2fraction[g_start:g_end]
+        galaxy.sf_gas_mass = np.sum(h2_masses) * Msun
+        if galaxy.sf_gas_mass > 0:
+            galaxy.sf_gas_metallicity = (
+                np.sum(h2_masses * g_metals[g_start:g_end])
+                / galaxy.sf_gas_mass
+            )
+        else:
+            galaxy.sf_gas_metallicity = 0.0
+
+        galaxy.load_gas(
+            coordinates=g_coods[g_start:g_end] * kpc,
+            masses=g_masses[g_start:g_end] * Msun,
+            metallicities=g_metals[g_start:g_end],
+            smoothing_lengths=g_hsml[g_start:g_end] * kpc,
+            dust_masses=g_dustmass[g_start:g_end] * Msun,
         )
+        galaxies.append(galaxy)
 
     return galaxies
