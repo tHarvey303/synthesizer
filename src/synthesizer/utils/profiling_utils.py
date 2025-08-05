dafce75c1ad46702909757e403cbe9e3cc90963b
wjr21 <wjr21@sussex.ac.uk>
2025-08-05 20:21:27 +0100

    Actually just forget modifying the font

--- src/synthesizer/utils/profiling_utils.py
+++ src/synthesizer/utils/profiling_utils.py
@@ -16,8 +16,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.lines import Line2D
 
-plt.rcParams["font.family"] = "DeJavu Serif"
-plt.rcParams["font.serif"] = ["DeJavu Sans"]
 plt.rcParams["axes.labelsize"] = 8
 plt.rcParams["axes.titlesize"] = 8
 plt.rcParams["xtick.labelsize"] = 8
