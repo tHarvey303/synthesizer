d2b4899bbf04e22b010d79ade30d76c51fb55222
wjr21 <wjr21@sussex.ac.uk>
2025-08-06 16:35:32 +0100

    Ensuring we don't get mutliple prints

--- src/synthesizer/data/initialise.py
+++ src/synthesizer/data/initialise.py
@@ -413,7 +413,7 @@ class SynthesizerInitializer:
         print(f"{green}Synthesizer initialisation complete!{reset}\n")
 
 
-def synth_initialise() -> None:
+def synth_initialise(verbose=True) -> None:
     """Run the Synthesizer initialization process.
 
     This function runs the initialisation process. It creates the necessary
@@ -437,10 +437,11 @@ def synth_initialise() -> None:
 
     # Just exit if the data directory already exists
     if all_exist:
-        print(
-            "  🟢 Synthesizer data directory already exists, "
-            "no need to re-initialize."
-        )
+        if verbose:
+            print(
+                "  🟢 Synthesizer data directory already exists, "
+                "no need to re-initialize."
+            )
         return
 
     # Otherwise, create the initializer and run it, this will only make or copy
