7b4cc9b281af31d050d90c7815c394d8a6482612
Aswin P Vijayan <aswinpvijayan@github.com>
2025-10-27 17:59:50 +0000

    Update downloader with dust extinction grid

--- src/synthesizer/downloader/downloader.py
+++ src/synthesizer/downloader/downloader.py
@@ -183,7 +183,8 @@ def download_agn_test_grids(destination):
 
 
 def download_dust_grid(destination):
-    """Download the Drain and Li (2007) dust emission grid for synthesizer.
+    """Download the Draine and Li (2007) dust emission and extinction
+    grid for synthesizer.
 
     Args:
         destination (str):
@@ -191,7 +192,8 @@ def download_dust_grid(destination):
     """
     # Download the dust grid
     _download("draine_li_dust_emission_grid_MW_3p1.hdf5", destination)
-
+    _download("dust_extcurve_draine_li_lognormal_asmall0p01_alarge0p1_apah0p005.hdf5", destination)
+    
 
 def download_camels_data(destination):
     """Download the CAMELS data.
@@ -260,7 +262,7 @@ def download():
         "--dust-grid",
         "-D",
         action="store_true",
-        help="Download the dust grid for the Drain & Li (2007) model",
+        help="Download the dust grid for the Draine & Li models",
     )
 
     # Add the flag for processed camels data
@@ -335,7 +337,7 @@ def download():
     test = args.test_grids
     stellar = args.stellar_test_grids
     agn = args.agn_test_grids
-    dust = args.dust_grid
+    dust_grid = args.dust_grid
     camels = args.camels_data
     everything = args.all
     dest = args.destination
@@ -385,10 +387,10 @@ def download():
     if all_instruments:
         download_instruments(INSTRUMENT_CACHE_DIR, AVAILABLE_INSTRUMENTS)
 
-    # Dust data?
-    if dust:
+    # Dust emission data?
+    if dust_grid:
         download_dust_grid(dest if dest is not None else GRID_DIR)
-
+        
     # Camels data?
     if camels:
         download_camels_data(dest if dest is not None else TEST_DATA_DIR)
