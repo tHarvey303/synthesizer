ed83a9225cb2c9b8e8495ac5841ba9795e160f6a
wjr21 <wjr21@sussex.ac.uk>
2025-11-21 14:39:57 +0000

    Adding ParameterFunction machinery

--- src/synthesizer/emission_models/utils.py
+++ src/synthesizer/emission_models/utils.py
@@ -1,5 +1,7 @@
 """A submodule containing utility functions for the emission models."""
 
+import inspect
+
 import numpy as np
 
 from synthesizer import exceptions
@@ -78,6 +80,12 @@ def get_param(param, model, emission, emitter, obj=None, default=_NO_DEFAULT):
     # looking on the emitter at this point)
     if value is not None and isinstance(value, str):
         return get_param(value, None, None, emitter, default=default)
+
+    # If we found a ParameterFunction, call it to get the value
+    elif value is not None and isinstance(value, ParameterFunction):
+        return value(model, emission, emitter, obj)
+
+    # If we found a value, return it
     elif value is not None:
         return value
 
@@ -177,4 +185,132 @@ def get_params(params, model, emission, emitter, obj=None):
 
 
 class ParameterFunction:
-    """A class for wrapping functions that compute parameters for emitters."""
+    """A class for wrapping functions that compute parameters for emitters.
+
+    This class can be used to wrap functions which take emitter attributes
+    as inputs and return a computed parameter value or array of values. This
+    class is designed as a dependency injection mechanism to be passed to
+    EmissionModel arguments that require dynamic parameter computation from
+    an emitter. As such, this is mostly designed for internal use within the
+    Synthesizer package, but it can also be used by an experienced user to
+    create custom parameter functions.
+
+    Any function wrapped by this class must:
+        - Follow this signature: func(**kwargs) -> value
+        - Return a single value or numpy/unyt array of values. If an array is
+          returned, it must be the same shape as arrays on the emitter (i.e.
+          nstar in length for per star properties etc.).
+        - Have kwargs which are either attributes of the emitter object or
+          fixed parameters on an EmissionModel.
+        - Have kwargs which are all defined in the "func_args" list
+          (set during initialization).
+
+    Example:
+        def compute_metallicity(mass, age, fixed_param):
+            # Compute metallicity based on mass, age, and a fixed parameter
+            return (mass * 0.01) + (age * 0.001) + fixed_param
+
+        param_func = ParameterFunction(
+            func=compute_metallicity,
+            func_args=['mass', 'age', 'fixed_param']
+        )
+
+        # Define an emission model that fixes 'fixed_param' to 0.02
+        model = EmissionModel(
+            label='custom_model',
+            fixed_param=0.02,
+            grid=grid,
+            metallicity_param=param_func,
+        )
+
+        # Later... call get spectra on an emitter which will use the function
+        # to compute metallicity dynamically.
+        emitter.get_spectra(model)
+
+        # And you can see the cached value to was used
+        print(emitter.model_param_cache['custom_model']['metallicity_param'])
+    """
+
+    def __init__(self, func: callable, sets: str, func_args: list) -> None:
+        """Initialize the function wrapper.
+
+        This will attach the function and set the list of argument names
+        that the function takes ready for later extraction.
+
+        Args:
+            func (callable):
+                The function to wrap.
+            sets (str):
+                A string indicating the attribute on the emitter that this
+                function sets.
+            func_args (list):
+                A list of argument names that the function takes. These must
+                correspond to attributes on the emitter or fixed parameters
+                on an EmissionModel.
+
+        Raises:
+            ValueError:
+                If func is not callable.
+        """
+        if not callable(func):
+            raise ValueError("func must be a callable function.")
+
+        self.func = func
+        self.func_args = func_args
+
+        # Ensure the function signature matches the func_args
+        sig = inspect.signature(func)
+        for arg in func_args:
+            if arg not in sig.parameters:
+                raise exceptions.InconsistentArguments(
+                    f"Found func_arg '{arg}' on ParameterFunction which is "
+                    "not an argument of the wrapped function "
+                    f"'{func.__name__}'."
+                )
+        for param in sig.parameters:
+            if param not in func_args:
+                raise exceptions.InconsistentArguments(
+                    f"Found argument '{param}' on the wrapped function "
+                    f"'{func.__name__}' which is not in the func_args list "
+                    "of the ParameterFunction."
+                )
+
+    def __call__(self, model, emission, emitter, obj=None):
+        """Call the wrapped function with parameters extracted from objects.
+
+        This will extract the required parameters from the model, emission,
+        emitter, or optional object and call the wrapped function with those
+        parameters.
+
+        Args:
+            model (EmissionModel):
+                The model object.
+            emission (Sed/LineCollection):
+                The emission object.
+            emitter (Stars/Gas/Galaxy):
+                The emitter object.
+            obj (object, optional):
+                An optional additional object to look for parameters on last.
+
+        Returns:
+            value:
+                The value returned by the wrapped function.
+        """
+        # Extract the required parameters
+        func_kwargs = {}
+        for arg in self.func_args:
+            func_kwargs[arg] = get_param(
+                arg,
+                model,
+                emission,
+                emitter,
+                obj,
+            )
+
+        # Call the function with the extracted parameters
+        val = self.func(**func_kwargs)
+
+        # Cache the computed value on the emitter for later use
+        emitter.model_param_cache.setdefault(model.label, {})[self.sets] = val
+
+        return val
