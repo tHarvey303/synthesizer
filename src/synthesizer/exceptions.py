127d1d00f86a06d9d95794de7c2e8bd385d01492
wjr21 <wjr21@sussex.ac.uk>
2025-12-11 09:17:39 +0000

    Hash exception

--- src/synthesizer/exceptions.py
+++ src/synthesizer/exceptions.py
@@ -874,3 +874,28 @@ class ParameterFunctionError(Exception):
         if self.message:
             return "{0} ".format(self.message)
         return "There was an error in the parameter function."
+
+
+class CannottHashThat(Exception):
+    """Exception class for when an object cannot be hashed."""
+
+    def __init__(self, *args):
+        """Initialise the exception with an optional message.
+
+        Args:
+            *args: Optional message to include in the exception.
+        """
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        """Return the string representation of the exception.
+
+        Returns:
+            str: The string representation of the exception.
+        """
+        if self.message:
+            return "{0} ".format(self.message)
+        return "The object cannot be hashed."
