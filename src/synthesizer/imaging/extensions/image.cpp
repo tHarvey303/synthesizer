42a1e63d699183e3a87f7465f2a33e60bf57ca3e
wjr21 <wjr21@sussex.ac.uk>
2026-01-13 22:09:35 +0000

    Setting an error message in the octree

--- src/synthesizer/imaging/extensions/image.cpp
+++ src/synthesizer/imaging/extensions/image.cpp
@@ -241,8 +241,6 @@ static void populate_pixel_recursive(const struct cell *c, double threshold,
 
     /* Safety check: ensure we have valid dimensions. */
     if (local_width <= 0 || local_height <= 0) {
-      PyErr_SetString(PyExc_ValueError,
-                      "Invalid local image dimensions calculated.");
       return;
     }
 
@@ -589,6 +587,11 @@ PyObject *make_img(PyObject *self, PyObject *args) {
       extract_data_double(np_smoothing_lengths, "smoothing_lengths");
   const double *pos = extract_data_double(np_pos, "pos");
   const double *kernel = extract_data_double(np_kernel, "kernel");
+
+  if (pix_values == NULL || smoothing_lengths == NULL || pos == NULL ||
+      kernel == NULL) {
+    return NULL;
+  }
 
   toc("Extracting Python data", start_time);
 
